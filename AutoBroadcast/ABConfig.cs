067b7536910e522095625a211a847ebcfd243a51
Controllerdestiny <523321293@qq.com>
2024-04-10 12:19:09 +0800

    Add AutoBroadcast Plugin

---
+++ AutoBroadcast/ABConfig.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace AutoBroadcast
+{
+	public class ABConfig
+	{
+		public Broadcast[] Broadcasts = new Broadcast[0];
+
+		public ABConfig Write(string file)
+		{
+			File.WriteAllText(file, JsonConvert.SerializeObject(this, Formatting.Indented));
+			return this;
+		}
+
+		public static ABConfig Read(string file)
+		{
+			if (!File.Exists(file))
+			{
+				ABConfig.WriteExample(file);
+			}
+			return JsonConvert.DeserializeObject<ABConfig>(File.ReadAllText(file));
+		}
+
+		public static void WriteExample(string file)
+		{
+			File.WriteAllText(file, @"{
+			  ""Broadcasts"": [
+				{
+				  ""Name"": ""Example Broadcast"",
+				  ""Enabled"": false,
+				  ""Messages"": [
+					""This is an example broadcast"",
+					""It will broadcast every 5 minutes"",
+					""Broadcasts can also execute commands"",
+					""/time noon""
+				  ],
+				  ""ColorRGB"": [
+					255.0,
+					0.0,
+					0.0
+				  ],
+				  ""Interval"": 300,
+				  ""StartDelay"": 60,
+				  ""Groups"": [],
+				  ""TriggerWords"": [],
+				  ""TriggerToWholeGroup"": false
+				}
+			  ]
+			}");
+		}
+	}
+
+	public class Broadcast
+	{
+		public string Name = string.Empty;
+		public bool Enabled = false;
+		public string[] Messages = new string[0];
+		public float[] ColorRGB = new float[3];
+		public int Interval = 0;
+		public int StartDelay = 0;
+		public string[] Groups = new string[0];
+		public string[] TriggerWords = new string[0];
+		public bool TriggerToWholeGroup = false;
+	}
+}
--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 ### 目前已收集插件
 
-| 名称            |     插件说明     |  作者  |
-| --------------- | :--------------: | :----: |
-| ChattyBridge    |   用于跨服聊天   | 少司命 |
-| EconomicsAPI    |   经济插件前置   | 少司命 |
-| Economics.RPG   |       RPG        | 少司命 |
-| Economics.Deal  |     交易插件     | 少司命 |
-| Economics.Shop  |     商店插件     | 少司命 |
-| Economics.Skill | 技能插件(未完成) | 少司命 |
-| CreateSpawn     |  出生点建筑生成  | 少司命 |
+| 名称            |     插件说明     |   作者    |
+| --------------- | :--------------: | :-------: |
+| ChattyBridge    |   用于跨服聊天   |  少司命   |
+| EconomicsAPI    |   经济插件前置   |  少司命   |
+| Economics.RPG   |       RPG        |  少司命   |
+| Economics.Deal  |     交易插件     |  少司命   |
+| Economics.Shop  |     商店插件     |  少司命   |
+| Economics.Skill | 技能插件(未完成) |  少司命   |
+| CreateSpawn     |  出生点建筑生成  |  少司命   |
+| AutoBroadcast   |     自动广播     | Scavenger |
