7178a2179e2f494fe09c98329cfdbbe926a76fd3
xien <2383759126@qq.com>
2024-04-13 16:17:25 +0800

    添加插件：RegionView

---
+++ RegionView/RegionPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+
+namespace RegionView
+{
+    public class RegionPlayer
+    {
+		public int Index { get; }
+
+		public TSPlayer TSPlayer 
+		{ 
+			get
+				=> TShock.Players[Index];
+		}
+
+		public List<Region> Regions { get; } = new();
+
+		public bool IsViewingNearby { get; set; }
+
+		public RegionPlayer(int index) 
+			=> Index = index;
+	}
+}
