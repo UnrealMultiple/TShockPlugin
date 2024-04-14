1069b5fbe4d695e9ef8ea82cce1498ab6dc4bf89
xien <2383759126@qq.com>
2024-04-14 12:11:42 +0800

    添加插件：GolfRewards 高尔夫奖励

---
+++ GolfRewards/LC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using TShockAPI;
+
+namespace RewardSection
+{
+    internal class LC
+    {
+        public static ConfigFile LConfig { get; set; }
+        internal static string LConfigPath => Path.Combine(TShock.SavePath, "高尔夫奖励.json");
+        public static LPlayer[] LPlayers { get; set; }
+        public static Random LRadom = new Random();
+
+        public static void RI()
+        {
+            LConfig = new ConfigFile();
+            LPlayers = new LPlayer[256];
+        }
+
+        public static void RC()
+        {
+            try
+            {
+                if (!File.Exists(LConfigPath))
+                {
+                    TShock.Log.ConsoleError("未找到高尔夫奖励配置文件，已为您创建！");
+                }
+                LConfig = ConfigFile.Read(LConfigPath);
+                LConfig.Write(LConfigPath);
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.ConsoleError("高尔夫奖励插件错误配置读取错误:" + ex.ToString());
+            }
+        }
+    }
+}
