b85e2030d2612c9c21e923473468f25f154a8ae5
羽学 <1242509682@qq.com>
2024-04-16 04:24:19 +0800

    新增了子命令可快速设置玩家与竞技场的界限，配置文件加了个【离开竞技场杀死玩家】的选项并对配置文件进行汉化，修复了逃离竞技场判定死亡认输。

--- PvPer/PvPer.cs
+++ PvPer/PvPer.cs
@@ -3,6 +3,7 @@ using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using TShockAPI.Hooks;
+using Microsoft.Xna.Framework;
 
 namespace PvPer
 {
@@ -10,43 +11,77 @@ namespace PvPer
     public class PvPer : TerrariaPlugin
     {
         public override string Name => "决斗系统";
-        public override Version Version => new Version(1, 0, 2);
+        public override Version Version => new Version(1, 0, 3);
         public override string Author => "Soofa 羽学修改";
         public override string Description => "不是你死就是我活系列";
-        public PvPer(Main game) : base(game) { }
-        public static string ConfigPath = Path.Combine(TShock.SavePath + "/决斗系统.json");
-        public static Config Config = new Config();
+        public PvPer(Main game) : base(game)
+        {
+
+            var configPath = "决斗系统.json";
+            var configuration = Configuration.Read(configPath);
+        }
+        public static Configuration Config = new Configuration();
+        public static DbManager DbManager = new DbManager(new SqliteConnection("Data Source=" + Path.Combine(TShock.SavePath, "决斗系统.sqlite")));
         public static List<Pair> Invitations = new List<Pair>();
         public static List<Pair> ActiveDuels = new List<Pair>();
-        public static DbManager DbManager = new DbManager(new SqliteConnection("Data Source=" + Path.Combine(TShock.SavePath, "决斗系统.sqlite")));
+
         public override void Initialize()
         {
+            LoadConfig();
             GetDataHandlers.PlayerTeam += OnPlayerChangeTeam;
             GetDataHandlers.TogglePvp += OnPlayerTogglePvP;
             GetDataHandlers.Teleport += OnPlayerTeleport;
             GetDataHandlers.PlayerUpdate += OnPlayerUpdate;
             GetDataHandlers.KillMe += OnKill;
             ServerApi.Hooks.ServerLeave.Register(this, OnServerLeave);
-            GeneralHooks.ReloadEvent += OnReload;
-            TShockAPI.Commands.ChatCommands.Add(new Command("pvper.admin", ClearAllDataCmd, "决斗重置","repvp")
+            GeneralHooks.ReloadEvent += LoadConfig;
+            TShockAPI.Commands.ChatCommands.Add(new Command("pvper.use", Commands.Duel, "决斗", "pvp"));
+            TShockAPI.Commands.ChatCommands.Add(new Command("pvper.admin", ClearAllDataCmd, "决斗重置", "repvp")
             {
                 AllowServer = true, // 允许服务器端使用
                 HelpText = "清除数据库中所有玩家的数据（仅限管理员）",
             });
 
-            TShockAPI.Commands.ChatCommands.Add(new Command("pvper.use", Commands.Duel, "决斗", "pvp"));
-            Config = Config.Read();
+        }
 
+        private static void LoadConfig(ReloadEventArgs args = null!)
+        {
+            string configPath = Configuration.FilePath;
+
+            if (File.Exists(configPath))
+            {
+                Config = Configuration.Read(configPath);
+                Console.WriteLine($"[决斗系统]已重载");
+            }
+            else
+            {
+                Config = new Configuration();
+                Config.Write(configPath);
+            }
         }
 
         #region Hooks
+
         public static void OnPlayerUpdate(object? sender, GetDataHandlers.PlayerUpdateEventArgs args)
         {
-            if (Utils.IsPlayerInADuel(args.PlayerId) && !Utils.IsPlayerInArena(args.Player))
+            TSPlayer plr = TShock.Players[args.PlayerId];
+
+            if (Utils.IsPlayerInADuel(args.PlayerId) && !Utils.IsPlayerInArena(plr))
             {
-                args.Player.DamagePlayer(int.MaxValue);
+                if (PvPer.Config.KillPlayer)
+                {
+                    args.Player.KillPlayer();
+                    plr.SendMessage("你已离开竞技场，系统已自动判定怯战 惩罚为：[c/F86565:失败并死亡]。", Color.Yellow);
+                }
+                else
+                {
+                    args.Player.DamagePlayer(int.MaxValue);
+                    plr.SendMessage("你已离开竞技场，系统已自动判定怯战 惩罚为：[c/F86565:失败并扣血]。", Color.Yellow);
+                }
             }
         }
+
+
         public void OnKill(object? sender, GetDataHandlers.KillMeEventArgs args)
         {
             TSPlayer plr = TShock.Players[args.PlayerId];
@@ -83,7 +118,6 @@ namespace PvPer
         }
         public static void OnPlayerTeleport(object? sender, GetDataHandlers.TeleportEventArgs args)
         {
-            TSPlayer plr = TShock.Players[args.ID];
             Pair? duel = Utils.GetDuel(args.ID);
 
             if (duel != null && !Utils.IsLocationInArena((int)(args.X / 16), (int)(args.Y / 16)))
@@ -104,13 +138,9 @@ namespace PvPer
                 plr.SendData(PacketTypes.PlayerTeam, number: plr.Index);
             }
         }
-        private static void OnReload(ReloadEventArgs args)
-        {
-            args.Player.SendSuccessMessage("决斗系统已重载");
-            Config = Config.Read();
-        }
+        #endregion
 
-        //清理玩家数据库表的方法
+        #region 使用指令清理数据库、设置位置方法
         private void ClearAllDataCmd(CommandArgs args)
         {
             // 权限
@@ -120,7 +150,6 @@ namespace PvPer
                 TShock.Log.ConsoleInfo("玩家试图执行重置决斗系统数据指令");
                 return;
             }
-
             // 尝试从数据库中删除所有玩家数据
             if (DbManager.ClearData())
             {
