4a0373e3590df0cf879d602298d13ffa5d6265de
羽学 <1242509682@qq.com>
2024-05-17 04:06:04 +0800

    更新：v1.2.1 加入了对指令开关的破坏保护，与该开关的独有说明，汉化了配置文件

--- SwitchCommands/Database.cs
+++ SwitchCommands/Database.cs
@@ -8,8 +8,19 @@ namespace SwitchCommands
     {
         public static string databasePath = Path.Combine(TShock.SavePath, "开关配置表.json");
 
+        [JsonProperty("是否开启开关保护", Order = -3)]
+        public bool SwitchEnable = true;
+
+        [JsonProperty("试图破坏开关的警告", Order = -3)]
+        public string SwitchText = "你没有权限破坏指令开关！";
+
+        [JsonProperty("添加指令时的前缀", Order = -3)]
+        public string SwitchPrefix = "/'";
+
+        [JsonProperty("开关指令表")]
         public Dictionary<string, CommandInfo> switchCommandList = new Dictionary<string, CommandInfo>();
 
+        #region 读取配置文件方法
         public void Write(string path)
         {
             File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
@@ -18,8 +29,22 @@ namespace SwitchCommands
         public static Database Read(string path)
         {
             if (!File.Exists(path))
-                return new Database();
-            return JsonConvert.DeserializeObject<Database>(File.ReadAllText(path));
+            {
+                var c = new Database();
+                c.Write(path);
+                return c;
+            }
+            else
+            {
+                using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (var sr = new StreamReader(fs))
+                {
+                    var json = sr.ReadToEnd();
+                    var cf = JsonConvert.DeserializeObject<Database>(json);
+                    return cf!;
+                }
+            }
         }
+        #endregion
     }
 }
