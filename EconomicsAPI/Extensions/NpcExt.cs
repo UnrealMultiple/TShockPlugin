b0e2f46893982fdab75ca2aa582f8fe16d08477d
Controllerdestiny <523321293@qq.com>
2024-05-19 16:40:12 +0800

    更新: EconomicsAPI 添加更多扩展函数

---
+++ EconomicsAPI/Extensions/NpcExt.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+
+namespace EconomicsAPI.Extensions;
+
+public static class NpcExt
+{
+    public static List<NPC> GetNpcInRange(this NPC npc, int range)
+    {
+        return Main.npc.Where(n =>
+        {
+            float dx = n.position.X - npc.position.X;
+            float dy = n.position.Y - npc.position.Y;
+            return (dx * dx) + (dy * dy) <= range * range * 256f;
+        }).ToList();
+    }
+}
