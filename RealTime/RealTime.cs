809177d4fae8d4b3ea99eab8404da5bc51938ab6
十七 <62046588+1653638943@users.noreply.github.com>
2024-06-16 02:19:52 +0800

    更新：

--- RealTime/RealTime.cs
+++ RealTime/RealTime.cs
@@ -130,7 +130,7 @@ namespace RealTime
                 if (Main.pumpkinMoon)
                 {
                     //万圣节开启瞬间
-                    time = DateTime.Now + TimeSpan.FromMinutes(59);
+                    time = DateTime.Now + TimeSpan.FromMinutes(40);
                 }
             }
             lastPumpkinMoon = Main.pumpkinMoon;
@@ -158,7 +158,7 @@ namespace RealTime
                 if (Main.snowMoon)
                 {
                     //霜月开启瞬间
-                    time = DateTime.Now + TimeSpan.FromMinutes(59);
+                    time = DateTime.Now + TimeSpan.FromMinutes(40);
                 }
             }
             lastSnowMoon = Main.snowMoon;
@@ -205,7 +205,7 @@ namespace RealTime
             #endregion
             #region npc生成、月相、天气、渔夫任务刷新
             y++;
-            if (y == 54000)//npc生成 月相、天气、渔夫任务刷新
+            if (y == 86400)//npc生成 月相、天气、渔夫任务刷新
             {
                 var AllNPCS = Main.npc.Where(n => n != null);
                 foreach (var TNPC in AllNPCS)
