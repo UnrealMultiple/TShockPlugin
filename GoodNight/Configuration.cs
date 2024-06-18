3d4c8f66514d618a2343eeecf8add9fca1f8c255
羽学 <1242509682@qq.com>
2024-06-18 14:01:16 +0800

    更新：宵禁v2.2.0 加入指令功能与权限检查

--- GoodNight/Configuration.cs
+++ GoodNight/Configuration.cs
@@ -1,11 +1,12 @@
 ﻿using Newtonsoft.Json;
 using System.Text;
 using TShockAPI;
+using static Org.BouncyCastle.Math.EC.ECCurve;
 
-public struct TimeRange
+public class TimeRange
 {
     public TimeSpan Start { get; set; }
-    public TimeSpan End { get; set; }
+    public TimeSpan Stop { get; set; }
 }
 
 namespace Goodnight
@@ -21,51 +22,48 @@ namespace Goodnight
         public bool DiscPlayers = false;
         [JsonProperty("玩家进服拦截消息", Order = -9)]
         public string JoinMessage = "当前为宵禁时间，无法加入游戏。";
-        [JsonProperty("玩家攻击断连消息", Order = -9)]
+        [JsonProperty("踢出玩家断连消息", Order = -9)]
         public string NewProjMessage = "到点了，晚安";
         [JsonProperty("断连豁免玩家", Order = -9)]
-        public List<string> ExemptPlayers = new List<string>();
+        public List<string> PlayersList { get; set; }
 
         [JsonProperty("禁怪少于人数(设1为关闭禁怪)", Order = -8)]
         public int MaxPlayers { get; set; } = 2;
         [JsonProperty("宵禁时间设置(禁怪/断连)", Order = -7)]
-        public TimeRange Time { get; set; } = new TimeRange { Start = TimeSpan.FromHours(0), End = TimeSpan.FromHours(5) };
+        public TimeRange Time { get; set; } = new TimeRange()
+        {
+            Start = TimeSpan.FromHours(0),
+            Stop = TimeSpan.FromHours(5)
+        };
+
         [JsonProperty("禁止怪物生成表(NpcID)", Order = -6)]
         public HashSet<int> Npcs = new();
 
+        #region 读取与创建配置文件方法
         public Configuration()
         {
-            Npcs = new HashSet<int>() { 4, 13, 14, 15, 35, 36, 37, 50, 113, 114, 125, 126, 127, 128, 129, 130, 131, 134, 135, 136, 222, 245, 246, 247, 248, 249, 262, 266, 370, 396, 397, 398, 400, 439, 440, 422, 493, 507, 517, 636, 657, 668 };
+            PlayersList = new List<string>();
         }
 
-        #region 读取与创建配置文件方法
         public void Write(string path)
         {
             using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
             using (var sw = new StreamWriter(fs, new UTF8Encoding(false)))
-            {
-                var str = JsonConvert.SerializeObject(this, Formatting.Indented);
-                sw.Write(str);
-            }
+                sw.Write(JsonConvert.SerializeObject(this, Formatting.Indented));
         }
 
         public static Configuration Read(string path)
         {
             if (!File.Exists(path))
             {
-                var c = new Configuration();
-                c.Write(path);
-                return c;
+                new Configuration().Write(path);
+                return new Configuration();
             }
             else
             {
                 using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
                 using (var sr = new StreamReader(fs))
-                {
-                    var json = sr.ReadToEnd();
-                    var cf = JsonConvert.DeserializeObject<Configuration>(json);
-                    return cf!;
-                }
+                    return JsonConvert.DeserializeObject<Configuration>(sr.ReadToEnd())!;
             }
         }
         #endregion
