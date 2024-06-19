390afffa1d4ee0d8bfda7b6555695646ffbdce69
羽学 <1242509682@qq.com>
2024-06-19 08:16:18 +0800

    更新：宵禁 v2.3.0 加入在宵禁时间内允许可召唤已击败怪物

--- GoodNight/Goodnight.cs
+++ GoodNight/Goodnight.cs
@@ -1,10 +1,9 @@
-﻿using Newtonsoft.Json;
-using System.IO;
-using Terraria;
+﻿using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using TShockAPI.Hooks;
 using static TShockAPI.GetDataHandlers;
+using Terraria.ID;
 
 namespace Goodnight
 {
@@ -14,7 +13,7 @@ namespace Goodnight
         #region 变量与插件信息
         public override string Name => "宵禁";
         public override string Author => "Jonesn 羽学";
-        public override Version Version => new Version(2, 2, 1);
+        public override Version Version => new Version(2, 3, 0);
         public override string Description => "设置服务器无法进入或禁止生成怪物的时段";
         internal static Configuration Config;
         #endregion
@@ -29,8 +28,9 @@ namespace Goodnight
             GeneralHooks.ReloadEvent += LoadConfig;
             ServerApi.Hooks.NpcSpawn.Register(this, OnSpawn);
             ServerApi.Hooks.NpcTransform.Register(this, OnTransform);
+            ServerApi.Hooks.NpcKilled.Register(this, OnNPCKilled);
             ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
-            TShockAPI.Commands.ChatCommands.Add(new Command("goodnight.admin", Commands.GnCmd, "gn","宵禁"));
+            TShockAPI.Commands.ChatCommands.Add(new Command("goodnight.admin", Commands.GnCmd, "gn", "宵禁"));
         }
 
         protected override void Dispose(bool disposing)
@@ -41,8 +41,10 @@ namespace Goodnight
                 GeneralHooks.ReloadEvent -= LoadConfig;
                 ServerApi.Hooks.NpcSpawn.Deregister(this, OnSpawn);
                 ServerApi.Hooks.NpcTransform.Deregister(this, OnTransform);
+                ServerApi.Hooks.NpcKilled.Deregister(this, OnNPCKilled);
                 ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
-                
+                TShockAPI.Commands.ChatCommands.Remove(new Command("goodnight.admin", Commands.GnCmd, "gn", "宵禁"));
+
             }
             base.Dispose(disposing);
         }
@@ -57,16 +59,10 @@ namespace Goodnight
                 Config = new Configuration();
                 Config.Write();
             }
-
             else
-            {
                 Config = Configuration.Read();
-            }
-
             if (args != null && args.Player != null)
-            {
                 args.Player.SendSuccessMessage("[宵禁]重新加载配置完毕。");
-            }
         }
         #endregion
 
@@ -100,37 +96,98 @@ namespace Goodnight
         #region 禁止召唤怪物
         private void OnSpawn(NpcSpawnEventArgs args)
         {
+            int PlayerCount = TShock.Utils.GetActivePlayerCount();
+            bool Npcs = Config.Npcs.Contains(Main.npc[args.NpcId].netID);
+            bool NpcDie = Config.NpcDie.Contains(Main.npc[args.NpcId].netID);
+            bool NoPlr = PlayerCount < Config.MaxPlayers && Config.MaxPlayers > 0;
+
             if (args.Handled || !Config.Enabled) return;
+
             else if (DateTime.Now.TimeOfDay >= Config.Time.Start && DateTime.Now.TimeOfDay < Config.Time.Stop)
             {
-                int PlayerCount = TShock.Utils.GetActivePlayerCount();
-                if (PlayerCount < Config.MaxPlayers && Config.MaxPlayers > 0)
+                if (NoPlr)
                 {
-                    if (Config.Npcs.Contains(Main.npc[args.NpcId].netID))
+                    if (NpcDie)
+                    {
+                        args.Handled = false;
+                        Main.npc[args.NpcId].active = true;
+                        TShock.Utils.Broadcast($"允许召唤已击败的怪物为："
+                            + string.Join(", ", Config.NpcDie.Select
+                            (x => TShock.Utils.GetNPCById(x)?.FullName + "({0})".SFormat(x))),
+                            Microsoft.Xna.Framework.Color.AntiqueWhite);
+                    }
+
+                   else if (Npcs)
                     {
                         args.Handled = true;
                         Main.npc[args.NpcId].active = false;
                         TShock.Utils.Broadcast($"【宵禁】当前服务器处于维护时间\n" +
-                            $"在线人数少于[c/FF3A4B:{Config.MaxPlayers}人]禁止[c/338AE1:召唤]怪物\n" +
+                            $"在线人数少于[c/FF3A4B:{Config.MaxPlayers}人]或该怪物[c/338AE1:不允许召唤]\n" +
                             $"禁止召唤怪物时间: " +
                             $"[c/DF95EC:{Config.Time.Start}] — [c/FF9187:{Config.Time.Stop}]", Microsoft.Xna.Framework.Color.AntiqueWhite);
                     }
+                }
+
+                else
+                {
+                    if (Npcs)
+                        args.Handled = false;
+                    Main.npc[args.NpcId].active = true;
                 }
             }
         }
 
         private void OnTransform(NpcTransformationEventArgs args)
         {
+            int PlayerCount = TShock.Utils.GetActivePlayerCount();
+            bool NpcDie = Config.NpcDie.Contains(Main.npc[args.NpcId].netID);
+            bool Npcs = Config.Npcs.Contains(Main.npc[args.NpcId].netID);
+            bool NoPlr = PlayerCount <= Config.MaxPlayers && Config.MaxPlayers > 0;
+
             if (args.Handled || !Config.Enabled) return;
             else if (DateTime.Now.TimeOfDay >= Config.Time.Start && DateTime.Now.TimeOfDay < Config.Time.Stop)
             {
-                int PlayerCount = TShock.Utils.GetActivePlayerCount();
-                if (PlayerCount < Config.MaxPlayers && Config.MaxPlayers > 0)
+                if (NoPlr)
                 {
-                    if (Config.Npcs.Contains(Main.npc[args.NpcId].netID))
+                    if (NpcDie)
+                    {
+                        Main.npc[args.NpcId].active = true;
+                    }
+
+                   else if (Npcs)
                     {
                         Main.npc[args.NpcId].active = false;
                     }
+                }
+
+                else
+                {
+                    if (Npcs)
+                    Main.npc[args.NpcId].active = true;
+                }
+            }
+        }
+
+        private void OnNPCKilled(NpcKilledEventArgs args)
+        {
+            int killNpc = args.npc.netID;
+
+            if (killNpc == 398)
+            {
+                Config.NpcDie.Clear();
+                Config.Write();
+                TShock.Utils.Broadcast($"玩家已击败了月亮领主，清空已击败进度记录", Microsoft.Xna.Framework.Color.AntiqueWhite);
+            }
+            else if (Config.Npcs.Contains(killNpc))
+            {
+                if (!Config.NpcDie.Contains(killNpc))
+                {
+                    Config.NpcDie.Add(killNpc);
+                    Config.Write();
+                    TShock.Utils.Broadcast($"NPC: {killNpc} 已被击败并记录可召唤怪物中：\n"
+                        + string.Join(", ", Config.NpcDie.Select
+                        (x => TShock.Utils.GetNPCById(x)?.FullName + "({0})".SFormat(x))),
+                        Microsoft.Xna.Framework.Color.AntiqueWhite);
                 }
             }
         }
