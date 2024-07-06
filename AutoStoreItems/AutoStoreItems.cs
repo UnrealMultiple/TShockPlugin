5385c5d74454821c3687b9eac9364531abe3abce
羽 学 <1242509682@qq.com>
2024-07-06 08:06:19 +0800

    添加插件：自动储存 v1.2.1

--- AutoStoreItems/AutoStoreItems.cs
+++ AutoStoreItems/AutoStoreItems.cs
@@ -4,7 +4,6 @@ using TerrariaApi.Server;
 using TShockAPI;
 using TShockAPI.Hooks;
 
-
 namespace Plugin
 {
     [ApiVersion(2, 1)]
@@ -14,7 +13,7 @@ namespace Plugin
         #region 插件信息
         public override string Name => "自动存储";
         public override string Author => "羽学";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 2, 1);
         public override string Description => "涡轮增压不蒸鸭";
         #endregion
 
@@ -23,15 +22,15 @@ namespace Plugin
         public override void Initialize()
         {
             LoadConfig();
-            ServerApi.Hooks.GameUpdate.Register(this, OnGameUpdate);
             GeneralHooks.ReloadEvent += (_) => LoadConfig();
+            ServerApi.Hooks.GameUpdate.Register(this, OnGameUpdate);
         }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                ServerApi.Hooks.GameUpdate.Deregister(this, OnGameUpdate);
                 GeneralHooks.ReloadEvent -= (_) => LoadConfig();
+                ServerApi.Hooks.GameUpdate.Deregister(this, OnGameUpdate);
             }
             base.Dispose(disposing);
         }
@@ -42,8 +41,29 @@ namespace Plugin
         private static void LoadConfig()
         {
             Config = Configuration.Read();
+            WriteName();
             Config.Write();
             TShock.Log.ConsoleInfo("[自动存储]重新加载配置完毕。");
+        }
+        #endregion
+
+        #region 获取物品ID的中文名
+        private static void WriteName()
+        {
+            foreach (var item in Config.Items)
+            {
+                var Names = new HashSet<string>(item.Name.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries));
+                foreach (var id in item.ID)
+                {
+                    string ItemName;
+                    ItemName = (string)Lang.GetItemName(id);
+                    if (!Names.Contains(ItemName))
+                    {
+                        Names.Add(ItemName);
+                    }
+                }
+                item.Name = string.Join(", ", Names);
+            }
         }
         #endregion
 
@@ -62,23 +82,32 @@ namespace Plugin
                         for (int i = 0; i < plr.TPlayer.inventory.Length; i++)
                         {
                             var inv = plr.TPlayer.inventory[i];
+                            bool HandSwitch = Config.Hand ? plr.TPlayer.inventory[plr.TPlayer.selectedItem].type == item : inv.type == item ;
 
-                            if (inv.type == item) 
-                            { 
-                                AutoStoreItem(plr);
+                            if (HandSwitch)
+                            {
+                                bool Stored = false;  //用来防止4个储存空间有同样物品刷堆叠用
+                                if (Config.bank1 && !Stored)
+                                    Stored = AutoStoredItem(plr);
+                                if (Config.bank2 && !Stored)
+                                    Stored = AutoStoredItem2(plr);
+                                if (Config.bank3 && !Stored)
+                                    Stored = AutoStoredItem3(plr);
+                                if (Config.bank4 && !Stored)
+                                    Stored = AutoStoredItem4(plr);
+                                if (Stored) break;
                             }
                         }
                     }
                 }
-
             });
         }
         #endregion
 
-        #region 自动存储方法
-        public static void AutoStoreItem(TSPlayer args)
+        #region 自动存储方法(存钱罐）
+        public static bool AutoStoredItem(TSPlayer args)
         {
-            if (!args.IsLoggedIn || !Config.Enable) return;
+            if (!args.IsLoggedIn || !Config.Enable || !Config.bank1) return false;
 
             Player plr = args.TPlayer;
             foreach (var item in Config.Items)
@@ -86,22 +115,180 @@ namespace Plugin
                 for (int b = 0; b < plr.bank.item.Length; b++)
                 {
                     var bank = plr.bank.item[b];
+
                     for (int i = 0; i < plr.inventory.Length; i++)
                     {
                         var inv = plr.inventory[i];
                         if (inv.stack >= item.Stack
-                            && inv.type == item.ID
+                            && Array.Exists(item.ID, id => id == inv.type)
                             && inv.type == bank.type
-                            && bank.type == item.ID)
+                            && Array.Exists(item.ID, id => id == bank.type)
+                            && Array.Exists(item.ID, id => id != plr.inventory[plr.selectedItem].type))
                         {
+
                             bank.stack += inv.stack;
                             inv.TurnToAir();
                             args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Inventory0 + i);
                             args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Bank1_0 + b);
+
+                            if (Config.Mess)
+                                args.SendMessage($"【自动储存】已将'[c/92C5EC:{bank.Name}]'存入您的存钱罐 当前数量: {bank.stack}", 255, 246, 158);
+                            return true;
                         }
+
+                        else if (Config.clear && bank.stack >= 9999)
+                        {
+                            bank.stack = 9999 - 1;
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Bank1_0 + b);
+                            if (Config.Mess)
+                                args.SendMessage($"【自动储存】检测到'[c/92C5EC:{bank.Name}]'已达到 [c/F7575C:{bank.stack}] 请整理你的存钱罐", 255, 246, 158);
+                        }
+
                     }
                 }
             }
+            return false;
+        }
+        #endregion
+
+        #region 自动存储方法（保险箱）
+        public static bool AutoStoredItem2(TSPlayer args)
+        {
+            if (!args.IsLoggedIn || !Config.Enable || !Config.bank2) return false;
+
+            Player plr = args.TPlayer;
+            foreach (var item in Config.Items)
+            {
+                for (int b = 0; b < plr.bank2.item.Length; b++)
+                {
+                    var bank = plr.bank2.item[b];
+
+                    for (int i = 0; i < plr.inventory.Length; i++)
+                    {
+                        var inv = plr.inventory[i];
+                        if (inv.stack >= item.Stack
+                           && Array.Exists(item.ID, id => id == inv.type)
+                           && inv.type == bank.type
+                           && Array.Exists(item.ID, id => id == bank.type)
+                           && Array.Exists(item.ID, id => id != plr.inventory[plr.selectedItem].type))
+                        {
+
+                            bank.stack += inv.stack;
+                            inv.TurnToAir();
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Inventory0 + i);
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Bank2_0 + b);
+
+                            if (Config.Mess)
+                                args.SendMessage($"【自动储存】已将'[c/92C5EC:{bank.Name}]'存入您的保险箱 当前数量: {bank.stack}", 255, 246, 158);
+                            return true;
+                        }
+
+                        else if (Config.clear && bank.stack >= 9999)
+                        {
+                            bank.stack = 9999 - 1;
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Bank2_0 + b);
+                            if (Config.Mess)
+                                args.SendMessage($"【自动储存】检测到'[c/92C5EC:{bank.Name}]'已达到 [c/F7575C:{bank.stack}] 请整理你的保险箱", 255, 246, 158);
+                        }
+
+                    }
+                }
+            }
+            return false;
+        }
+        #endregion
+
+        #region 自动存储方法（护卫熔炉）
+        public static bool AutoStoredItem3(TSPlayer args)
+        {
+            if (!args.IsLoggedIn || !Config.Enable || !Config.bank3) return false;
+
+            Player plr = args.TPlayer;
+            foreach (var item in Config.Items)
+            {
+                for (int b = 0; b < plr.bank3.item.Length; b++)
+                {
+                    var bank = plr.bank3.item[b];
+
+                    for (int i = 0; i < plr.inventory.Length; i++)
+                    {
+                        var inv = plr.inventory[i];
+                        if (inv.stack >= item.Stack
+                            && Array.Exists(item.ID, id => id == inv.type)
+                            && inv.type == bank.type
+                            && Array.Exists(item.ID, id => id == bank.type)
+                            && Array.Exists(item.ID, id => id != plr.inventory[plr.selectedItem].type))
+                        {
+
+                            bank.stack += inv.stack;
+                            inv.TurnToAir();
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Inventory0 + i);
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Bank3_0 + b);
+
+                            if (Config.Mess)
+                                args.SendMessage($"【自动储存】已将'[c/92C5EC:{bank.Name}]'存入您的护卫熔炉 当前数量: {bank.stack}", 255, 246, 158);
+                            return true;
+                        }
+
+                        else if (Config.clear && bank.stack >= 9999)
+                        {
+                            bank.stack = 9999 - 1;
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Bank3_0 + b);
+                            if (Config.Mess)
+                                args.SendMessage($"【自动储存】检测到'[c/92C5EC:{bank.Name}]'已达到 [c/F7575C:{bank.stack}] 请整理你的护卫熔炉", 255, 246, 158);
+                        }
+
+                    }
+                }
+            }
+            return false;
+        }
+        #endregion
+
+        #region 自动存储方法（虚空袋）
+        public static bool AutoStoredItem4(TSPlayer args)
+        {
+            if (!args.IsLoggedIn || !Config.Enable || !Config.bank4) return false;
+
+            Player plr = args.TPlayer;
+            foreach (var item in Config.Items)
+            {
+                for (int b = 0; b < plr.bank4.item.Length; b++)
+                {
+                    var bank = plr.bank4.item[b];
+
+                    for (int i = 0; i < plr.inventory.Length; i++)
+                    {
+                        var inv = plr.inventory[i];
+                        if (inv.stack >= item.Stack
+                           && Array.Exists(item.ID, id => id == inv.type)
+                           && inv.type == bank.type
+                           && Array.Exists(item.ID, id => id == bank.type)
+                           && Array.Exists(item.ID, id => id != plr.inventory[plr.selectedItem].type))
+                        {
+
+                            bank.stack += inv.stack;
+                            inv.TurnToAir();
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Inventory0 + i);
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Bank4_0 + b);
+
+                            if (Config.Mess)
+                                args.SendMessage($"【自动储存】已将'[c/92C5EC:{bank.Name}]'存入您的虚空袋 当前数量: {bank.stack}", 255, 246, 158);
+                            return true;
+                        }
+
+                        else if (Config.clear && bank.stack >= 9999)
+                        {
+                            bank.stack = 9999 - 1;
+                            args.SendData(PacketTypes.PlayerSlot, "", args.Index, PlayerItemSlotID.Bank4_0 + b);
+                            if (Config.Mess)
+                                args.SendMessage($"【自动储存】检测到'[c/92C5EC:{bank.Name}]'已达到 [c/F7575C:{bank.stack}] 请整理你的虚空袋", 255, 246, 158);
+                        }
+
+                    }
+                }
+            }
+            return false;
         }
         #endregion
     }
--- AutoStoreItems/Configuration.cs
+++ AutoStoreItems/Configuration.cs
@@ -1,32 +1,64 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using TShockAPI;
 
 namespace Plugin
 {
     internal class Configuration
     {
-
-        [JsonProperty("插件开关", Order = -10)]
+        [JsonProperty("插件开关", Order = -12)]
         public bool Enable = true;
+        [JsonProperty("储存速度", Order = -11)]
+        public float time { get; set; } = 120f;
 
-        [JsonProperty("持有物品", Order = 0)]
-        public int[] HoldItems { get; set; } = new int[1] { 87 };
+        [JsonProperty("使用说明1", Order = -10)]
+        public static string Text = "[是否手持] 需要选中 [持有物品] 其中1个才会启动存储功能，关闭则背包含有 其中1个就会启动";
 
-        [JsonProperty("储存速度", Order = 0)]
-        public float time { get; set; } = 10f;
+        [JsonProperty("使用说明2", Order = -10)]
+        public static string Text2 = "[储存速度] 不要低于60帧(推荐120)，否则手动 [连续] 快速放入 [同样物品到存储空间格子] 会导致物品数量翻倍";
 
-        [JsonProperty("存到存钱罐的物品表", Order = 1)]
+        [JsonProperty("使用说明3", Order = -10)]
+        public static string Text3 = "[控超堆叠] 会在物品超过9999的时候 对该物品减-1处理 然后通知玩家整理空间";
+
+        [JsonProperty("使用说明4", Order = -10)]
+        public static string Text4 = "[物品名] 会在使用 [/Reload] 指令时根据 [物品ID] 自动写入，[物品数量] 为储存最低数量要求 ";
+
+        [JsonProperty("使用说明5", Order = -10)]
+        public static string Text5 = "[存在BUG] 收藏的物品会被取消收藏(指虚空袋药水有堆叠进箱子的风险) ";
+
+        [JsonProperty("存钱罐", Order = -9)]
+        public bool bank1 = true;
+        [JsonProperty("保险箱", Order = -8)]
+        public bool bank2 = true;
+        [JsonProperty("护卫熔炉", Order = -6)]
+        public bool bank3 = true;
+        [JsonProperty("虚空袋", Order = -7)]
+        public bool bank4 = true;
+
+        [JsonProperty("控超堆叠", Order = -5)]
+        public bool clear = true;
+        [JsonProperty("存储提示", Order = -4)]
+        public bool Mess = true;
+
+        [JsonProperty("是否手持(↓)", Order = -1)]
+        public bool Hand = false;
+
+        [JsonProperty("持有物品", Order = 1)]
+        public int[] HoldItems { get; set; } = new int[7] { 87, 346 , 3213, 3813, 4076, 4131, 5325 };
+
+        [JsonProperty("存储物品表", Order = 3)]
         public List<ItemData> Items { get; set; } = new List<ItemData>();
+
 
         #region 预设参数方法
         public void Ints()
         {
             Items = new List<ItemData>
             {
-                new ItemData(71, 1, 0),
-                new ItemData(72, 1, 0),
-                new ItemData(73, 1, 0),
-                new ItemData(74, 1, 0)
+                new ItemData("", 20,new int[] { 23, }),
+                new ItemData("", 1, new int[] { 3817,71,72,73,74,75,1774,1869 })
+
             };
         }
         #endregion
@@ -34,20 +66,21 @@ namespace Plugin
         #region 数据结构
         public class ItemData
         {
-            [JsonProperty("物品ID", Order = 1)]
-            public int ID { get; set; }
+            [JsonProperty("物品名(不用写)", Order = 0)]
+            public string Name { get; set; } = "";
 
-            [JsonProperty("数量", Order = 2)]
+            [JsonProperty("物品数量", Order = 1)]
             public int Stack { get; set; }
 
-            [JsonProperty("前缀", Order = 3)]
-            public int Prefix { get; set; }
+            [JsonProperty("物品ID", Order = 2)]
+            public int[] ID { get; set; }
 
-            public ItemData( int id, int stack,int prefix)
+
+            public ItemData(string name = "", int stack = 0 , int[] id = null!)
             {
-                ID = id;
-                Stack = stack;
-                Prefix = prefix;
+                Name = name ?? "";
+                ID = id ?? new int[] { 9 };
+                Stack = Math.Max(stack, 1);
             }
         }
         #endregion
@@ -77,8 +110,6 @@ namespace Plugin
             }
         }
         #endregion
-
-
 
     }
 }
