e92458094d5ae5c75e1782d21d02cd392fdfd002
Controllerdestiny <523321293@qq.co>
2024-07-15 13:23:46 +0800

    补更版本号

--- EconomicsAPI/Economics.cs
+++ EconomicsAPI/Economics.cs
@@ -23,7 +23,7 @@ public class Economics : TerrariaPlugin
 
     public override string Name => Assembly.GetExecutingAssembly().GetName().Name!;
 
-    public override Version Version => Assembly.GetExecutingAssembly().GetName().Version!;
+    public override Version Version => new(1, 0, 0, 1);
 
     public readonly static List<TSPlayer> ServerPlayers = new();
 
