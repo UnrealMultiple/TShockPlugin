a90156c213c218c2f097a477b37ac0feec9fad87
Controllerdestiny <523321293@qq.co>
2024-07-19 13:34:59 +0800

    补更版本号

--- Economics.Skill/Skill.cs
+++ Economics.Skill/Skill.cs
@@ -20,7 +20,7 @@ public class Skill : TerrariaPlugin
 
     public override string Name => Assembly.GetExecutingAssembly().GetName().Name!;
 
-    public override Version Version => new(1, 2, 0, 6);
+    public override Version Version => new(1, 2, 0, 7);
 
     internal static string PATH = Path.Combine(EconomicsAPI.Economics.SaveDirPath, "Skill.json");
 
