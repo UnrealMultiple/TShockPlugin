0aa04bc933a3bd8dfa5968764df002f8027276e2
Cai <13110818005@qq.com>
2024-07-25 11:33:16 +0800

    更新: CaiBot并行处理数据包，以防止API调用超时

--- CaiBot/MessageHandler.cs
+++ CaiBot/MessageHandler.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using NuGet.Protocol;
 using Rests;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
