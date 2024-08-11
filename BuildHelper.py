9fb0389f389a1935203a0316c163d4c48445d594
Cai <62058454+ACaiCat@users.noreply.github.com>
2024-08-11 10:09:37 +0800

    工作流: 优化工作流程，打包结构，添加使用前说明 (#383)
    * 工作流: 优化工作流程，打包结构，添加使用前说明

--- BuildHelper.py
+++ BuildHelper.py
@@ -3,22 +3,20 @@ import glob
 import shutil
 import sys
 import zipfile
-import urllib.request
 
 def zip_files_in_folder(folder_path, zip_file_path):
     with zipfile.ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zipf:
-        for foldername, subfolders, filenames in os.walk(folder_path):
+        for foldername, _, filenames in os.walk(folder_path):
             for filename in filenames:
                 file_path = os.path.join(foldername, filename)
                 zipf.write(file_path, arcname=os.path.basename(file_path))
     print(f"📦 压缩包已生成: {zip_file_path}")
     
-def md_to_pdf(file_name):
-    os.system(f"pandoc --pdf-engine=xelatex  -V mainfont=LXGWWenKaiMono-Regular.ttf -V geometry:margin=0.5in --template eisvogel.tex  {file_name} -o {file_name.replace('.md', '.pdf')}")
-    
+
 if __name__ == '__main__':
     print(f"🚀 开始执行打包脚本...(By Cai 😋)")
     build_type = sys.argv[1]
+
     print(f"🗑️ 开始删除json文件...")
     for file in glob.glob(os.path.join(f"out/{build_type}/", "*.json")):
         os.remove(file)
@@ -29,38 +27,37 @@ if __name__ == '__main__':
     cwd = os.getcwd()
     for dir_name in os.listdir(cwd):
         dir_path = os.path.join(cwd, dir_name)
-        if os.path.isdir(dir_path):
-            for file_name in os.listdir(dir_path):
-                try:
-                    if file_name.endswith('.csproj'):
-                        source_path = os.path.join(dir_path, 'README.md')
-                        destination_path = os.path.join(cwd, 'out', f'{build_type}', file_name.replace('.csproj', '.md'))
-                        shutil.copyfile(source_path, destination_path)
-                        print(f"🔍 找到README.md({destination_path})")
-                except:
-                    print(f"⚠️ README移动失败({file_name})")
+        for file_name in os.listdir(dir_path):
+            try:
+                if file_name.endswith('.csproj'):
+                    source_path = os.path.join(dir_path, 'README.md')
+                    destination_path = os.path.join(cwd, 'out', f'{build_type}', file_name.replace('.csproj', '.md'))
+                    shutil.copyfile(source_path, destination_path)
+                    print(f"🔍 找到README.md({destination_path})")
+            except:
+                print(f"⚠️ README移动失败({file_name})")
+
+
+    os.makedirs(f'out/{build_type}/Plugins', exist_ok=True)
+
+    out_dir = f'out/{build_type}'
+    files = [f for f in os.listdir(out_dir) if os.path.isfile(os.path.join(out_dir, f))]
+
+
+    for file in files:
+        shutil.move(os.path.join(out_dir, file), os.path.join(f'out/{build_type}/Plugins', file))
                     
     shutil.copyfile('README.md', f"out/{build_type}/TShockPlugin.md")
     print("✅ README.md移动成功！")
+
+    shutil.copyfile('Useage.txt', f"out/{build_type}/使用前须知.txt")
+    print("✅ 使用前须知.txt移动成功！")
+
     shutil.copyfile('Plugins.json', f"out/{build_type}/Plugins.json")
     print("✅ Plugins.json移动成功！")
-    '''
-    if build_type == "Release":
-        print("🔄 准备转换PDF...")
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/lxgw/LxgwWenKai/main/fonts/TTF/LXGWWenKaiMono-Regular.ttf", "LXGWWenKaiMono-Regular.ttf")
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex", "eisvogel.tex")
-        directory = '/usr/share/texmf/fonts/opentype/public/lm/'
-        specified_file = 'LXGWWenKaiMono-Regular.ttf'
-        for filename in os.listdir(directory):
-            if os.path.isfile(os.path.join(directory, filename)):
-                shutil.copy2(specified_file, os.path.join(directory, filename))
-        for file_name in os.listdir(f"out/{build_type}"):
-            if file_name.endswith('.md'):
-                md_to_pdf(f"{cwd}/out/{build_type}/{file_name}")
-                os.remove(f"{cwd}/out/{build_type}/{file_name}")
-                print(f"✅ {file_name}转换成功...")
-        print("✅ PDF转换完成！")
-    '''
+
+    shutil.copyfile('LICENSE', f"out/{build_type}/LICENSE")
+    print("✅ 开源协议移动成功！")
 
     print("📦 准备打包插件...")
     zip_files_in_folder("out", "Plugins.zip")
