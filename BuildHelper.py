9113b74b2dde7a52d54e11c081720e266b514440
Cai <62058454+ACaiCat@users.noreply.github.com>
2024-08-11 10:23:05 +0800

    Update BuildHelper.py

--- BuildHelper.py
+++ BuildHelper.py
@@ -6,10 +6,11 @@ import zipfile
 
 def zip_files_in_folder(folder_path, zip_file_path):
     with zipfile.ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zipf:
-        for foldername, _, filenames in os.walk(folder_path):
+        for foldername, subfolders, filenames in os.walk(folder_path):
             for filename in filenames:
                 file_path = os.path.join(foldername, filename)
-                zipf.write(file_path, arcname=os.path.basename(file_path))
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname=arcname)
     print(f"📦 压缩包已生成: {zip_file_path}")
     
 
