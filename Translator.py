a69199e1436bab608dae0c7f6351aed68f5fcebf
少司命 <79201070+Controllerdestiny@users.noreply.github.com>
2024-08-26 22:12:50 +0800

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -7,3 +7,15 @@ for file in os.listdir("./"):
     if os.path.exists(path):
       cmd.append(file)
 subprocess.run(cmd)
+
+subprocess.run(["git", "config", "--local", "user.email", "action@github.com"])
+subprocess.run(["git", "config", "--local", "user.name", "GitHub Action"])
+
+for file in os.listdir("./"):
+   path = f"{file}/README.en.md"
+   if(os.path.exists(path)):
+      subprocess.run(["git", "add", path])
+
+subprocess.run(["git", "commit", "-m", "自动更新README.md"])
+subprocess.run(["git", "pull"])
+subprocess.run(["git", ["push"])
