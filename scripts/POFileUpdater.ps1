0d56ee4c8c18bd2e1a84a82b730d44d881227826
Controllerdestiny <523321293@qq.co>
2024-08-31 17:05:17 +0800

    更新脚本

--- scripts/POFileUpdater.ps1
+++ scripts/POFileUpdater.ps1
@@ -9,6 +9,7 @@ foreach ($p in @(ls $PSScriptRoot/../src/*/*.csproj))  {
     if (-not (Test-Path -Path "$($p.DirectoryName)/i18n" -PathType Container)) {
         if($Model -eq "auto"){
             New-Item -ItemType Directory -Path "$($p.DirectoryName)/i18n"
+        }
     }
     
     $pot = "$([System.IO.Path]::Combine($p.DirectoryName, "i18n", "template.pot"))"
