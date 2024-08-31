f39a5195b379511f9fe538f72901342f7b425893
Controllerdestiny <523321293@qq.co>
2024-08-31 18:56:34 +0800

    更新测试脚本

--- scripts/POFileUpdater.py
+++ scripts/POFileUpdater.py
@@ -4,7 +4,7 @@ import subprocess
 import sys
 
 if __name__ == '__main__':
-    model = sys.argv[1]
+    model = "auto"
     for csproj in [Path(x) for x in glob.glob(str(Path(sys.argv[0]).parent.parent) + '/src/*/*.csproj')]:
         i18n_dir = csproj.parent / 'i18n'
         if not i18n_dir.is_dir():
@@ -13,7 +13,8 @@ if __name__ == '__main__':
             else:
                 continue
 
-        subprocess.run(["dotnet", "tool", "run", "GetText.Extractor", "-s", str(csproj), "-t", str(i18n_dir / "template.pot")], shell=True)
+        subprocess.run("dotnet + tool run GetText.Extractor -s " + str(csproj) + " -t " + str(i18n_dir / "template.pot"))
+
 
         for po_file in [Path(x) for x in glob.glob(i18n_dir.as_posix() + '/*.po')]:
-            subprocess.run(["msgmerge", "--previous", "--update", str(po_file), str(i18n_dir / "template.pot")], shell=True)
+            subprocess.run(["msgmerge", "--previous", "--update", str(po_file), str(i18n_dir / "template.pot")])
