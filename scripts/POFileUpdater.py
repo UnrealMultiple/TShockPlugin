4ce9ff2eb84b0828fdff27b758a58ec6296570f7
LaoSparrow <laosparrow@outlook.com>
2024-09-01 14:10:38 +0800

    test scripts

--- scripts/POFileUpdater.py
+++ scripts/POFileUpdater.py
@@ -3,14 +3,11 @@ from pathlib import Path
 import subprocess
 import sys
 
-def run_cmd(cmdline, is_output = False):
-    p = subprocess.run(cmdline, capture_output=is_output)
-    if is_output:
-        out = p.stdout.decode()
-        err = p.stderr.decode()
-        if out != "" or err != "":
-            print(out)
-            print(err)
+def run_cmd(cmdline, capture_output = False):
+    p = subprocess.run(cmdline, capture_output=capture_output)
+    if capture_output:
+        return p.stdout.decode()
+    return None
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -18,18 +15,21 @@ if __name__ == '__main__':
         i18n_dir = csproj.parent / 'i18n'
         if not i18n_dir.is_dir():
             if "auto" in args:
-                if "debug" in args:
-                    print(f"[{i18n_dir}] creating directory...")
+                print(f"[{i18n_dir}] creating directory...")
                 i18n_dir.mkdir()
             else:
                 continue
                 
-        if "debug" in args:
-            print(f"[{csproj}] generating template.pot...")
+        print(f"[{csproj}] generating template.pot...")
+        pot_file = i18n_dir / "template.pot"
+        run_cmd(["dotnet", "tool", "run", "GetText.Extractor", "-u", "-o", "-s", str(csproj), "-t", str(pot_file)])
 
-        run_cmd(["dotnet", "tool", "run", "GetText.Extractor", "-u", "-o", "-s", str(csproj), "-t", str(i18n_dir / "template.pot")], "debug" in args)
+        pot_diff = [x for x in run_cmd(["git", "diff", "--numstat", str(pot_file)], True).split(' ') if x != ""]
+        if pot_diff[0] == "2" and pot_diff[1] == "2":
+            f"[{str(csproj)}] template.pot no diff except date changes, restoring..."
+            run_cmd(["git", "restore", str(pot_file)])
+            continue
 
         for po_file in [Path(x) for x in glob.glob(i18n_dir.as_posix() + '/*.po')]:
-            if "debug" in args:
-                print(f"[{po_file}] merging...")
-            subprocess.run(["msgmerge", "--previous", "--update", str(po_file), str(i18n_dir / "template.pot")], "debug" in args)
+            print(f"[{po_file}] merging...")
+            run_cmd(["msgmerge", "--previous", "--update", str(po_file), str(i18n_dir / "template.pot")])
--- scripts/POFileUpdater.sh
+++ scripts/POFileUpdater.sh
@@ -2,15 +2,29 @@
 
 shopt -s nullglob
 for proj in $(dirname $0)/../src/*/*.csproj; do
-  if [ ! -d "$(dirname $proj)/i18n" ]; then
+  if [[ ! -d "$(dirname $proj)/i18n" ]]; then
     continue
   fi
 
+  echo "[$proj] generating template.pot..."
   pushd $(dirname $proj) > /dev/null
 
   dotnet tool run GetText.Extractor -u -o -s ./ -t i18n/template.pot
+  if [[ ! -f i18n/template.pot ]]; then
+    popd > /dev/null
+    continue 
+  fi
+  
+  pot_diff=($(git diff --numstat i18n/template.pot))
+  if [[ $pot_diff[0] == '2' ]] && [[ $pot_diff[1] == '2' ]]; then
+    echo "[$proj] template.pot no diff except date changes, restoring..."
+    git restore i18n/template.pot
+    popd > /dev/null
+    continue
+  fi
   
   for pofile in ./i18n/*.po; do
+    echo "[$pofile] merging..."
     msgmerge --previous --update $pofile i18n/template.pot
   done
 
