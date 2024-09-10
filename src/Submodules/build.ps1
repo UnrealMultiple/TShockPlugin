4208d8d702d877fb55df25b49f4625e8ebac46a8
Controllerdestiny <523321293@qq.co>
2024-09-10 21:31:37 +0800

    移动README文件

--- src/Submodules/build.ps1
+++
@@ -1,9 +0,0 @@
-
-
-$jsonContent = Get-Content -Path ".config/submodule_build.json" -Raw  | ConvertFrom-Json
-
-foreach($submodule in $jsonContent.submodules)
-{
-    $command = "dotnet build $submodule.project_path"
-    & $command
-}
