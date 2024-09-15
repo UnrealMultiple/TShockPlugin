0873e04a0cb2785c72dbbef92800005cc8448b9d
LaoSparrow <laosparrow@outlook.com>
2024-09-15 12:00:53 +0800

    refactor(RandRespawn): use queue to store player waiting for respawn

--- src/RandRespawn/MainPlugin.cs
+++ src/RandRespawn/MainPlugin.cs
@@ -13,7 +13,7 @@ public class MainPlugin : TerrariaPlugin
     public override string Description => "随机出生点，任何回到出生点的操作都会被随机传送";
     public override Version Version => new Version(1, 0, 3);
 
-    private readonly List<TSPlayer> RandSpawns = new List<TSPlayer>();
+    private readonly Queue<TSPlayer> RespawnQueue = new();
     public MainPlugin(Main game) : base(game)
     {
 
@@ -27,12 +27,10 @@ public class MainPlugin : TerrariaPlugin
 
     private void OnUpdate(EventArgs args)
     {
-        for (var i = this.RandSpawns.Count - 1; i >= 0; i--)
+        while (this.RespawnQueue.TryDequeue(out var player))
         {
-            var ply = this.RandSpawns[i];
             var (x, y) = this.Search();
-            ply.Teleport(x * 16, y * 16);
-            this.RandSpawns.RemoveAt(i);
+            player.Teleport(x * 16, y * 16);
         }
     }
 
@@ -48,7 +46,7 @@ public class MainPlugin : TerrariaPlugin
 
     private void OnSpawn(object? sender, GetDataHandlers.SpawnEventArgs args)
     {
-        this.RandSpawns.Add(args.Player);
+        this.RespawnQueue.Enqueue(args.Player);
     }
 
     private (int x, int y) Search()
@@ -59,10 +57,10 @@ public class MainPlugin : TerrariaPlugin
             x = this.rand.Next(0, Main.maxTilesX - 2);
             y = this.rand.Next(0, Main.maxTilesY - 2);
         }
-        while (!Avaiable(x, y));
+        while (!IsTileAvailable(x, y));
         return (x, y);
     }
-    private static bool Avaiable(int x, int y)
+    private static bool IsTileAvailable(int x, int y)
     {
         return
         !Main.tile[x + 0, y + 0].active() && !Main.tile[x + 1, y + 0].active() &&
