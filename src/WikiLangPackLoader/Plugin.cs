3c33ab05e5d9423d4b2790f1bf417a80f92c402e
Cai <13110818005@qq.com>
2024-09-16 19:09:29 +0800

    fix

--- src/WikiLangPackLoader/Plugin.cs
+++ src/WikiLangPackLoader/Plugin.cs
@@ -1,39 +1,45 @@
 ﻿using Microsoft.Xna.Framework;
+using On.Terraria.Localization;
 using ReLogic.Content.Sources;
 using System.IO.Compression;
 using System.Reflection;
 using Terraria;
 using Terraria.IO;
-using Terraria.Localization;
 using TerrariaApi.Server;
 using TShockAPI.Hooks;
+using GameCulture = Terraria.Localization.GameCulture;
 
 namespace WikiLangPackLoader;
 
 [ApiVersion(2, 1)]
 public class WikiLangPackLoader : TerrariaPlugin
 {
+    public WikiLangPackLoader(Main game)
+        : base(game)
+    {
+        this.Order = int.MaxValue;
+    }
 
     public override string Author => "Cai";
 
     public override string Description => "加载Wiki语言包";
 
     public override string Name => "中文Wiki语言包加载器";
-    public override Version Version => new Version(2024, 9, 16, 0);
+    public override Version Version => new (2024, 9, 16, 1);
 
-    public WikiLangPackLoader(Main game)
-    : base(game)
-    {
-        this.Order = int.MaxValue;
-    }
     public override void Initialize()
     {
-
-        On.Terraria.Localization.LanguageManager.SetLanguage_GameCulture += this.LanguageManager_SetLanguage_GameCulture;
+        LanguageManager.SetLanguage_GameCulture += this.LanguageManager_SetLanguage_GameCulture;
         //On.Terraria.Localization.LanguageManager.SetLanguage_int += LanguageManager_SetLanguage_int;
         //On.Terraria.Localization.LanguageManager.SetLanguage_string += LanguageManager_SetLanguage_string;
-        LanguageManager.Instance.SetLanguage(7);
+        Terraria.Localization.LanguageManager.Instance.SetLanguage(7);
+        ServerApi.Hooks.GamePostInitialize.Register(this, this.OnGamePostInitialize, int.MinValue);
         GeneralHooks.ReloadEvent += this.GeneralHooks_ReloadEvent;
+    }
+
+    private void OnGamePostInitialize(EventArgs args)
+    {
+        this.Load();
     }
 
     private void GeneralHooks_ReloadEvent(ReloadEventArgs e)
@@ -42,10 +48,8 @@ public class WikiLangPackLoader : TerrariaPlugin
     }
 
 
-
-    private void LanguageManager_SetLanguage_GameCulture(On.Terraria.Localization.LanguageManager.orig_SetLanguage_GameCulture orig, LanguageManager self, GameCulture culture)
+    private void LanguageManager_SetLanguage_GameCulture(LanguageManager.orig_SetLanguage_GameCulture orig, Terraria.Localization.LanguageManager self, GameCulture culture)
     {
-
         orig(self, culture);
         if (culture.LegacyId == 7)
         {
@@ -56,7 +60,6 @@ public class WikiLangPackLoader : TerrariaPlugin
             Console.ForegroundColor = ConsoleColor.Red; // 设置前景色为红色
             Console.BackgroundColor = ConsoleColor.Yellow; // 设置背景色为黄色
             Console.WriteLine("\n[中文Wiki语言包加载器]语言包跳过加载：Terraria处于非中文状态，可能是插件修改导致");
-
             Console.ResetColor(); // 重置为默认颜色
         }
     }
@@ -72,20 +75,18 @@ public class WikiLangPackLoader : TerrariaPlugin
         {
             resourceStream.CopyTo(fileStream);
         }
+
         Utils.TryCreatingDirectory(@"tshock/LangResourcePack/");
         ZipFile.ExtractToDirectory(filePath, @"tshock/LangResourcePack/", true);
         File.Delete(filePath);
         var pack = new ResourcePack(services, @"tshock/LangResourcePack/");
-        var list = new List<IContentSource>
-        {
-            pack.GetContentSource()
-        };
-        LanguageManager.Instance.UseSources(list);
+        var list = new List<IContentSource> { pack.GetContentSource() };
+        Terraria.Localization.LanguageManager.Instance.UseSources(list);
 
         Console.ForegroundColor = ConsoleColor.Green; // 设置前景色为红色
         Console.WriteLine("[中文Wiki语言包加载器]语言包已经加载！\n" +
-            $"作者：{pack.Author}\n" +
-            $"版本：{pack.Version.Major}.{pack.Version.Minor}");
+                          $"作者：{pack.Author}\n" +
+                          $"版本：{pack.Version.Major}.{pack.Version.Minor}");
 
         Console.ResetColor(); // 重置为默认颜色
     }
@@ -94,13 +95,13 @@ public class WikiLangPackLoader : TerrariaPlugin
     {
         if (disposing)
         {
-            On.Terraria.Localization.LanguageManager.SetLanguage_GameCulture -= this.LanguageManager_SetLanguage_GameCulture;
+            LanguageManager.SetLanguage_GameCulture -= this.LanguageManager_SetLanguage_GameCulture;
             //On.Terraria.Localization.LanguageManager.SetLanguage_int += LanguageManager_SetLanguage_int;
             //On.Terraria.Localization.LanguageManager.SetLanguage_string += LanguageManager_SetLanguage_string;
             GeneralHooks.ReloadEvent -= this.GeneralHooks_ReloadEvent;
+            ServerApi.Hooks.GamePostInitialize.Deregister(this, this.OnGamePostInitialize);
         }
+
         base.Dispose(disposing);
     }
-
-
 }
