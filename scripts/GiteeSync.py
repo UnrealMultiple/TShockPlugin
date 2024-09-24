8fa2fd08b3f7921a895262cf1436fda51ddca781
Controllerdestiny <523321293@qq.co>
2024-09-24 16:54:10 +0800

    gitee同步

--- scripts/GiteeSync.py
+++ scripts/GiteeSync.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+import os
 
 gitee_token = sys.argv[1]
 owner = 'kksjsj'
@@ -8,18 +9,21 @@ release_id = 431070
 headers = {
     'Authorization': f'token {gitee_token}'
 }
-files = {
-    'file': open('out/Plugins.zip', 'rb')
-}
+
 attachments = requests.get(f"https://gitee.com/api/v5/repos/{owner}/{repo}/releases/{release_id}/attach_files?&page=1&per_page=100&direction=asc",headers=headers).json()
 for attachment in attachments:
     response = requests.delete(f"https://gitee.com/api/v5/repos/{owner}/{repo}/releases/{release_id}/attach_files/{attachment['id']}",headers=headers)
     print(f'🗑️ 附件 {attachment["name"]} 删除{"成功" if response.status_code == 204 else "失败"}：{response.status_code}')
 
-response = requests.post(f"https://gitee.com/api/v5/repos/{owner}/{repo}/releases/{release_id}/attach_files", headers=headers, files=files)
-if response.status_code == 201:
-    print('✨ Gitee插件包上传成功！')
-    print('下载链接:', response.json()['browser_download_url'])
-else:
-    print('❓Gitee插件包上传失败:', response.status_code)
-    print(response.json())
+for root, _, files in os.walk("./bin"):
+    for file in files:
+        files = {
+            'file': open(os.path.join(root, file), 'rb')
+        }
+        response = requests.post(f"https://gitee.com/api/v5/repos/{owner}/{repo}/releases/{release_id}/attach_files", headers=headers, files=files)
+        if response.status_code == 201:
+            print('✨ Gitee插件包上传成功！')
+            print('下载链接:', response.json()['browser_download_url'])
+        else:
+            print('❓Gitee插件包上传失败:', response.status_code)
+            print(response.json())
