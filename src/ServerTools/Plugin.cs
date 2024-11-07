c33e87bfb814dbe3d46733d37b30168160ed2dbb
Controllerdestiny <523321293@qq.co>
2024-11-07 18:21:00 +0800

    版本号与README

--- src/ServerTools/Plugin.cs
+++ src/ServerTools/Plugin.cs
@@ -20,7 +20,7 @@ public partial class Plugin : TerrariaPlugin
 
     public override string Name => "ServerTools";// 插件名字
 
-    public override Version Version => new(1, 1, 7, 4);// 插件版本
+    public override Version Version => new(1, 1, 7, 5);// 插件版本
 
     private static Config Config = new();
 
