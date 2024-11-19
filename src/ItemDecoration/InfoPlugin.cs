377b8e3ac7bedc60f7fb58b3bbd5e8dd64e2a728
xien <2383759126@qq.com>
2024-11-19 19:18:19 +0800

    添加插件：ItemDeco 手持物品对话框和浮动文字显示

---
+++ src/ItemDecoration/InfoPlugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItemTextPlugin.Information
+{
+    public class InfoPlugin
+    {
+        public string Name => "ItemDecoration";
+        public string Author => "FrankV22, Soofa";
+        public string Description => "Show Item Decoration and More!!!";
+        public Version Version => new Version(2, 1, 0);
+    }
+}
