465fe4240f0ea527e2f8499cfd8ac4793f35a8dc
Controllerdestiny <523321293@qq.co>
2024-11-21 21:30:09 +0800

    update: Refactoring Plugin ItemDecoration

---
+++ src/ItemDecoration/Configured/BaseItemOption.cs
@@ -0,0 +1,13 @@
+﻿using LazyAPI.ConfigFiles;
+
+namespace ItemDecoration.Configured;
+public class BaseItemOption
+{
+    [LocalizedPropertyName(CultureType.Chinese, "显示名称")]
+    [LocalizedPropertyName(CultureType.English, "showName")]
+    public bool ShowName { get; set; }
+
+    [LocalizedPropertyName(CultureType.Chinese, "显示伤害")]
+    [LocalizedPropertyName(CultureType.English, "showDamage")]
+    public bool ShowDamage { get; set; }
+}
