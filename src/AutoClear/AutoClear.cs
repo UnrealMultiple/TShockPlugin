4a48c23c9e2e6c771513ce9efbd9f17755df0ec6
xien <2383759126@qq.com>
2024-12-28 12:44:48 +0800

    fix

--- src/AutoClear/AutoClear.cs
+++ src/AutoClear/AutoClear.cs
@@ -12,7 +12,7 @@ public class Autoclear : LazyPlugin
     public override string Author => "大豆子[Mute适配1447]，肝帝熙恩更新";
     public override string Description => "智能扫地机";
     public override string Name => "智能自动扫地";
-    public override Version Version => new Version(1, 0, 6);
+    public override Version Version => new Version(1, 0, 7);
 
     private bool _sweepScheduled = false;
     private DateTime _sweepScheduledAt;
