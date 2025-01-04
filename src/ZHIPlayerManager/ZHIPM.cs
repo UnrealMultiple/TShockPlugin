4927112d8770917871e1ca5c4d7ca50e56181a57
Cai <13110818005@qq.com>
2025-01-04 23:42:12 +0800

    chore,docs(ZHIPM,APM): update version number and changelog

--- src/ZHIPlayerManager/ZHIPM.cs
+++ src/ZHIPlayerManager/ZHIPM.cs
@@ -15,7 +15,7 @@ public partial class ZHIPM : TerrariaPlugin
 
     public override string Name => "ZHIPlayerManager";
 
-    public override Version Version => new Version(1, 0, 1, 1);
+    public override Version Version => new Version(1, 0, 1, 2);
 
     #region 字段或属性
     /// <summary>
