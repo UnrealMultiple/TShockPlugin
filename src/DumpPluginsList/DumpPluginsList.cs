a9460e750f4c82812efedcf394daf419af6f998d
Controllerdestiny <523321293@qq.com>
2025-01-09 16:35:11 +0800

    Optimize code

--- src/DumpPluginsList/DumpPluginsList.cs
+++ src/DumpPluginsList/DumpPluginsList.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System.Reflection;
 using Terraria;
 using TerrariaApi.Server;
+using TShockAPI;
 
 namespace DumpPluginsList;
 
@@ -38,6 +39,7 @@ public class DumpPluginsList : TerrariaPlugin
                     .GetField("loadedAssemblies", BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)?
                     .GetValue(null))!
                 .ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
+            var des = this.GetDescriptions();
             File.WriteAllText("Plugins.json", JsonConvert.SerializeObject(ServerApi.Plugins
                 .OrderBy(p => p.Plugin.Order).ThenBy(p => p.Plugin.Name)
                 .Where(p => p.Plugin.GetType().Assembly != typeof(TShockAPI.TShock).Assembly && p.Plugin.GetType().Assembly != this.GetType().Assembly)
@@ -51,7 +53,7 @@ public class DumpPluginsList : TerrariaPlugin
                         Name = manifest?.Name ?? p.Plugin.Name,
                         Version = manifest?.Version ?? p.Plugin.Version,
                         Author = manifest?.Author ?? p.Plugin.Author,
-                        Description = this.GetDescription(p.Plugin),
+                        Description = des[p.Plugin],
                         AssemblyName = p.Plugin.GetType().Assembly.GetName().Name,
                         Path = dict.TryGetValue(p.Plugin.GetType().Assembly, out var name) ? name + ".dll" : null,
                         Dependencies = manifest?.Dependencies ?? p.Plugin.GetType().Assembly.GetReferencedAssemblies()
@@ -73,18 +75,25 @@ public class DumpPluginsList : TerrariaPlugin
         }
     }
 
-    public Dictionary<string, string> GetDescription(TerrariaPlugin plugin)
+    public Dictionary<TerrariaPlugin, Dictionary<string, string>> GetDescriptions()
     {
-        var dic = new Dictionary<string, string>();
+        var dic = new Dictionary<TerrariaPlugin, Dictionary<string, string>>();
         var flag = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
-        foreach (var (langid, culture) in Terraria.Localization.GameCulture._legacyCultures)
+        var func = typeof(I18n).GetMethod("GetCatalog", flag)!.CreateDelegate<Func<Catalog>>();
+        Terraria.Localization.GameCulture._legacyCultures.Keys.ForEach(i =>
         {
-            Terraria.Localization.LanguageManager.Instance.SetLanguage(langid);
-            var func = typeof(I18n).GetMethod("GetCatalog", flag)!.CreateDelegate<Func<Catalog>>();
+            Terraria.Localization.LanguageManager.Instance.SetLanguage(i);
             var c = func();
             var langName = c.CultureInfo.Name == "zh-Hans" ? "zh-CN" : c.CultureInfo.Name;
-            dic[langName] = c.GetString(plugin.Description);
-        }
+            ServerApi.Plugins.ForEach(p =>
+            {
+                if (!dic.TryGetValue(p.Plugin, out var des) || des == null)
+                { 
+                    dic[p.Plugin] = new Dictionary<string, string>();
+                }
+                dic[p.Plugin][langName] = c.GetString(p.Plugin.Description);
+            });
+        });
         return dic;
     }
 }
