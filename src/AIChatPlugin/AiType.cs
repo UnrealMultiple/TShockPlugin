19fbdd6ccc72ac8bc5c916916ec83165666f532a
Controllerdestiny <523321293@qq.com>
2025-01-10 16:01:43 +0800

    Optimize your code

---
+++ src/AIChatPlugin/AiType.cs
@@ -0,0 +1,14 @@
+﻿namespace AIChatPlugin;
+
+public enum AiType
+{
+    /// <summary>
+    /// 默认
+    /// </summary>
+    Default,
+
+    /// <summary>
+    /// 速度模式
+    /// </summary>
+    Fast
+}
