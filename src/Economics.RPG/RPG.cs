a1b3aadc20b98de5adf404ecdbc5da2213c0c7bd
Controllerdestiny <523321293@qq.com>
2025-01-20 17:57:56 +0800

    update rpg Sole occupation

--- src/Economics.RPG/RPG.cs
+++ src/Economics.RPG/RPG.cs
@@ -18,7 +18,7 @@ public class RPG : TerrariaPlugin
     public override string Description => GetString("提供RPG玩法!");
 
     public override string Name => System.Reflection.Assembly.GetExecutingAssembly().GetName().Name!;
-    public override Version Version => new Version(2, 0, 0, 2);
+    public override Version Version => new Version(2, 0, 0, 3);
 
     internal static Config Config { get; set; } = new Config();
 
