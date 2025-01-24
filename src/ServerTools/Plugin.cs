3505c696d653e7a6f9d387203ff5dc3281e6e6d3
Controllerdestiny <523321293@qq.com>
2025-01-24 12:55:48 +0800

    This command prompt can be hidden

--- src/ServerTools/Plugin.cs
+++ src/ServerTools/Plugin.cs
@@ -84,27 +84,6 @@ public partial class Plugin : LazyPlugin
         this.HandleCommandLine(Environment.GetCommandLineArgs());
     }
 
-    private void OthersCmd(CommandArgs args)
-    {
-        if (args.Parameters.Count != 2)
-        {
-            args.Player.SendErrorMessage(GetString("语法错误，正确语法:/oc [玩家名称] [指令]"));
-            return;
-        }
-        var target = args.Parameters[0];
-        var cmd = args.Parameters[1];
-        var player = TShock.Players.FirstOrDefault(p => p != null && p.Active && p.Name == target);
-        if (player == null)
-        {
-            args.Player.SendErrorMessage(GetString("目标玩家不在线无法执行此命令!"));
-            return;
-        }
-        player.tempGroup = new SuperAdminGroup();
-        Commands.HandleCommand(player, cmd);
-        player.tempGroup = null;
-        args.Player.SendSuccessMessage(GetString($"已为玩家{player.Name}执行命令`{cmd}`"));
-    }
-
     protected override void Dispose(bool disposing)
     {
         if (disposing)
