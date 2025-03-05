4b14396d8dbb242e85514a357a3d8a39fd97e669
JTL <3951759473@qq.com>
2025-03-05 15:11:50 +0800

    Update AIConfig.cs

--- src/AIChatPlugin/AIConfig.cs
+++ src/AIChatPlugin/AIConfig.cs
@@ -6,10 +6,10 @@ public class Configuration
 {
     #region 创建配置
     [JsonProperty("回答字数限制")] public int AIAnswerWordsLimit { get; set; } = 666;
-    [JsonProperty("回答自动换行的字数")] public int AIAnswerWithLinebreaks { get; set; } = 50;
+    [JsonProperty("回答自动换行字数")] public int AIAnswerWithLinebreaks { get; set; } = 50;
     [JsonProperty("上下文限制")] public int AIContextuallimitations { get; set; } = 10;
     [JsonProperty("超时时间")] public int AITimeoutPeriod { get; set; } = 100;
-    [JsonProperty("对话显示的名字")] public string AIName { get; set; } = "AI";
+    [JsonProperty("对话显示名字")] public string AIName { get; set; } = "AI";
     [JsonProperty("AI设定")] public string AISettings { get; set; } = "You're a concise and effective multilingual AI that is adept at summarizing complex questions in one sentence in the language of the questioner";
     public static readonly string FilePath = Path.Combine(TShock.SavePath, "AIChat.json");
     public static Configuration Config { get; private set; } = new Configuration();
