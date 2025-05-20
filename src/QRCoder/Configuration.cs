2018a2fe28992266d72928f00e8f8e736f6d13ac
xien <2383759126@qq.com>
2025-05-20 12:49:59 +0800

    update：允许自定义二维码墙壁种类和颜色

---
+++ src/QRCoder/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using LazyAPI.Attributes;
+using LazyAPI.ConfigFiles;
+
+namespace QRCoder;
+
+[Config]
+internal class Config : JsonConfigBase<Config>
+{
+    protected override string Filename => "QRCoder";
+
+    [LocalizedPropertyName(CultureType.Chinese, "底墙")]
+    [LocalizedPropertyName(CultureType.English, "BaseWall")]
+    public int BaseWall = 155;
+
+    [LocalizedPropertyName(CultureType.Chinese, "底颜色")]
+    [LocalizedPropertyName(CultureType.English, "BaseColor")]
+    public int BaseColor = 26;
+
+    [LocalizedPropertyName(CultureType.Chinese, "码墙")]
+    [LocalizedPropertyName(CultureType.English, "CodeWall")]
+    public int CodeWall = 155;
+
+    [LocalizedPropertyName(CultureType.Chinese, "码颜色")]
+    [LocalizedPropertyName(CultureType.English, "CodeColor")]
+    public int CodeColor = 29;
+
+    [LocalizedPropertyName(CultureType.Chinese, "是否刷夜明漆")]
+    [LocalizedPropertyName(CultureType.English, "IlluminantCoating")]
+    public bool isGlowPaintApplied = true;
+}
