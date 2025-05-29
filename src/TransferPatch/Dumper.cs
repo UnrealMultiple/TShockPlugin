fe395d4014784302203ba53eb803de0c622cd8f2
Controllerdestiny <523321293@qq.com>
2025-05-29 23:08:20 +0800

    优化代码

--- src/TransferPatch/Dumper.cs
+++ src/TransferPatch/Dumper.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 
 namespace TransferPatch;
@@ -8,6 +5,9 @@ namespace TransferPatch;
 public class Dumper(string file) : IDisposable
 {
     private readonly StreamWriter _sw = new(file);
+
+    private readonly static BindingFlags Flag = BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly;
+
     public void DumpType(Type rootType)
     {
         var processedTypes = new HashSet<Type>();
@@ -19,9 +19,7 @@ public class Dumper(string file) : IDisposable
 
     private void DumpTypeInternal(Type type, Assembly targetAssembly, HashSet<Type> processedTypes)
     {
-        // 跳过已处理类型、基础类型和非目标程序集类型
-        if (processedTypes.Contains(type) || 
-            IsBasicType(type) || 
+        if (processedTypes.Contains(type) ||
             type.Assembly != targetAssembly)
         {
             return;
@@ -29,14 +27,14 @@ public class Dumper(string file) : IDisposable
         processedTypes.Add(type);
 
         // 处理字段
-        foreach (var field in GetRelevantFields(type))
+        foreach (var field in type.GetFields(Flag))
         {
             this._sw.WriteLine($"\"{FormatTypeName(field.DeclaringType)}.{field.Name}\" : \"\"");
             this.ProcessMemberType(field.FieldType, targetAssembly, processedTypes);
         }
 
         // 处理属性
-        foreach (var prop in GetRelevantProperties(type))
+        foreach (var prop in type.GetProperties(Flag))
         {
             this._sw.WriteLine($"\"{FormatTypeName(prop.DeclaringType)}.{prop.Name}\" : \"\"");
             this.ProcessMemberType(prop.PropertyType, targetAssembly, processedTypes);
@@ -56,7 +54,7 @@ public class Dumper(string file) : IDisposable
             return;
         }
 
-        if (IsGenericCollection(memberType))
+        if (memberType.IsGenericType)
         { 
             foreach(var args in memberType.GetGenericArguments())
             {
@@ -74,12 +72,11 @@ public class Dumper(string file) : IDisposable
         }
     }
 
-    // 核心：格式化类型名称（处理泛型和嵌套类型）
     private static string FormatTypeName(Type? type)
     {
         if (type == null)
         {
-            return "null";
+            return string.Empty;
         }
 
         // 处理嵌套类型
@@ -93,8 +90,6 @@ public class Dumper(string file) : IDisposable
         {
             var genericDef = type.GetGenericTypeDefinition();
             var name = genericDef.Name;
-            
-            // 保留反引号和参数数量，去除具体类型参数
             var backtickIndex = name.IndexOf('`');
             if (backtickIndex > 0)
             {
@@ -104,57 +99,7 @@ public class Dumper(string file) : IDisposable
             return $"{type.Namespace}.{name}`{genericDef.GetGenericArguments().Length}";
         }
 
-        // 普通类型
         return type.FullName ?? type.Name;
-    }
-    
-    private static bool IsGenericCollection(Type type)
-    {
-        if (!type.IsGenericType)
-        {
-            return false;
-        }
-
-        var genericTypeDef = type.GetGenericTypeDefinition();
-        return genericTypeDef == typeof(List<>) ||
-               genericTypeDef == typeof(Dictionary<,>) ||
-               genericTypeDef == typeof(HashSet<>) ||
-               genericTypeDef == typeof(Queue<>) ||
-               genericTypeDef == typeof(Stack<>) ||
-               genericTypeDef == typeof(LinkedList<>) ||
-               genericTypeDef == typeof(SortedSet<>) ||
-               genericTypeDef == typeof(ICollection<>) ||
-               genericTypeDef == typeof(IEnumerable<>) ||
-               genericTypeDef == typeof(IList<>);
-    }
-
-    private static IEnumerable<FieldInfo> GetRelevantFields(Type type)
-    {
-        return type.GetFields(
-            BindingFlags.Public | 
-            BindingFlags.Instance |
-            BindingFlags.DeclaredOnly
-        );
-    }
-
-    private static IEnumerable<PropertyInfo> GetRelevantProperties(Type type)
-    {
-        return type.GetProperties(
-            BindingFlags.Public | 
-            BindingFlags.Instance | 
-            BindingFlags.DeclaredOnly
-        );
-    }
-
-    private static bool IsBasicType(Type type)
-    {
-        return type.IsPrimitive || 
-               type == typeof(string) || 
-               type == typeof(object) || 
-               type == typeof(ValueType) || 
-               type == typeof(Enum) || 
-               type == typeof(void) || 
-               type.IsEnum;
     }
 
     public void Dispose()
