8d406cb2d4aff29bb6515f524838062501ed63f5
Cai <13110818005@qq.com>
2025-07-18 23:23:30 +0800

    refactor(WikiLangPackLoader): 优化修改中文的方式

--- src/WikiLangPackLoader/Plugin.cs
+++ src/WikiLangPackLoader/Plugin.cs
@@ -1,40 +1,55 @@
-﻿using Microsoft.Xna.Framework;
-using On.Terraria.Localization;
+﻿using GetText;
+using Localizer;
+using Microsoft.Xna.Framework;
+using MonoMod.RuntimeDetour;
+using Newtonsoft.Json;
 using ReLogic.Content.Sources;
+using System.Globalization;
 using System.IO.Compression;
 using System.Reflection;
 using Terraria;
 using Terraria.IO;
+using Terraria.Localization;
 using TerrariaApi.Server;
+using TShockAPI;
 using TShockAPI.Hooks;
-using GameCulture = Terraria.Localization.GameCulture;
+using Utils = Terraria.Utils;
 
 namespace WikiLangPackLoader;
 
 [ApiVersion(2, 1)]
 public class WikiLangPackLoader : TerrariaPlugin
 {
-    public WikiLangPackLoader(Main game)
-        : base(game)
+    public WikiLangPackLoader(Main game) : base(game)
     {
-        this.Order = int.MaxValue;
+        // ReSharper disable once RedundantBaseQualifier
+        base.Order = int.MinValue;
     }
-
     public override string Author => "Cai";
 
     public override string Description => GetString("加载Wiki语言包");
 
-    public override string Name => System.Reflection.Assembly.GetExecutingAssembly().GetName().Name!;
-    public override Version Version => new Version(2025, 05, 18, 2);
+    public override string Name => Assembly.GetExecutingAssembly().GetName().Name!;
+    public override Version Version => new (2025, 05, 18, 2);
 
+    private readonly Hook _langHook = new (typeof(TShock).Assembly.GetType("TShockAPI.I18n")!.GetProperty(
+        "TranslationCultureInfo",
+        BindingFlags.NonPublic | BindingFlags.Static)!.GetGetMethod(nonPublic:true)!, ()=>  new CultureInfo("zh-CN"));
     public override void Initialize()
     {
-        LanguageManager.SetLanguage_GameCulture += this.LanguageManager_SetLanguage_GameCulture;
-        //On.Terraria.Localization.LanguageManager.SetLanguage_int += LanguageManager_SetLanguage_int;
-        //On.Terraria.Localization.LanguageManager.SetLanguage_string += LanguageManager_SetLanguage_string;
-        Terraria.Localization.LanguageManager.Instance.SetLanguage(7);
         ServerApi.Hooks.GamePostInitialize.Register(this, this.OnGamePostInitialize, int.MinValue);
         GeneralHooks.ReloadEvent += this.GeneralHooks_ReloadEvent;
+        if (LanguageManager.Instance.ActiveCulture != GameCulture.FromLegacyId(7))
+        {
+            LanguageManager.Instance.SetLanguage(7);
+        }
+        this._langHook.Apply();
+        var path = (string)typeof(TShock).Assembly.GetType("TShockAPI.I18n")!.GetProperty(
+            "TranslationsDirectory",
+            BindingFlags.NonPublic | BindingFlags.Static)!.GetValue(null)!;
+        typeof(TShock).Assembly.GetType("TShockAPI.I18n")!.GetField(
+            "C",
+            BindingFlags.Public | BindingFlags.Static)!.SetValue(null, new Catalog("TShockAPI", path, new CultureInfo("zh-CN")));
     }
 
     private void OnGamePostInitialize(EventArgs args)
@@ -47,29 +62,14 @@ public class WikiLangPackLoader : TerrariaPlugin
         this.Load();
     }
 
-
-    private void LanguageManager_SetLanguage_GameCulture(LanguageManager.orig_SetLanguage_GameCulture orig, Terraria.Localization.LanguageManager self, GameCulture culture)
-    {
-        orig(self, culture);
-        if (culture.LegacyId == 7)
-        {
-            this.Load();
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red; // 设置前景色为红色
-            Console.BackgroundColor = ConsoleColor.Yellow; // 设置背景色为黄色
-            Console.WriteLine("\n[中文Wiki语言包加载器]语言包跳过加载：Terraria处于非中文状态，可能是插件修改导致");
-            Console.ResetColor(); // 重置为默认颜色
-        }
-    }
+    
 
     private void Load()
     {
         var assembly = Assembly.GetExecutingAssembly();
         var services = new GameServiceContainer();
-        var resourceName = "WikiLangPackLoader.ResourcePack.zip";
-        var filePath = @"tshock/LangResourcePack.zip";
+        const string resourceName = "WikiLangPackLoader.ResourcePack.zip";
+        const string filePath = @"tshock/LangResourcePack.zip";
         using (var resourceStream = assembly.GetManifestResourceStream(resourceName)!)
         using (var fileStream = new FileStream(filePath, FileMode.Create))
         {
@@ -95,9 +95,6 @@ public class WikiLangPackLoader : TerrariaPlugin
     {
         if (disposing)
         {
-            LanguageManager.SetLanguage_GameCulture -= this.LanguageManager_SetLanguage_GameCulture;
-            //On.Terraria.Localization.LanguageManager.SetLanguage_int += LanguageManager_SetLanguage_int;
-            //On.Terraria.Localization.LanguageManager.SetLanguage_string += LanguageManager_SetLanguage_string;
             GeneralHooks.ReloadEvent -= this.GeneralHooks_ReloadEvent;
             ServerApi.Hooks.GamePostInitialize.Deregister(this, this.OnGamePostInitialize);
         }
