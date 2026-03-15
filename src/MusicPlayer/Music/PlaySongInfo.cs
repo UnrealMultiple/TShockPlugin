271b892212de4dbf212be19aebaa30ace1e9c82e
stardream <1011819146@qq.com>
2026-03-15 18:29:18 +0800

    Add SongName property and improve play completion logic

--- src/MusicPlayer/Music/PlaySongInfo.cs
+++ src/MusicPlayer/Music/PlaySongInfo.cs
@@ -6,32 +6,44 @@ internal class PlaySongInfo
 {
     public List<List<float>> Notes;
     public int Tempo;
+    public string? SongName;
 
     public bool PlayCompleted { get; private set; }
     public VirtualPerformer Performer { get; private set; }
     private int delta;
     private DateTime lastUpdate;
     private int noteIndex;
+    private bool completedTriggered;
+    
     public event Action<int>? OnCompleted;
-    public PlaySongInfo(List<List<float>> notes, int tempo, VirtualPerformer performer)
+
+    public PlaySongInfo(List<List<float>> notes, int tempo, VirtualPerformer performer, string? songName = null)
     {
         this.Notes = notes;
         this.Tempo = tempo;
         this.Performer = performer;
+        this.SongName = songName;
     }
+
     public void Update(int index)
     {
         if (this.PlayCompleted)
         {
             return;
         }
+        
         if (this.noteIndex == this.Notes.Count)
         {
-            this.PlayCompleted = true;
-            this.OnCompleted?.Invoke(index);
+            if (!this.completedTriggered)
+            {
+                this.completedTriggered = true;
+                this.PlayCompleted = true;
+                this.OnCompleted?.Invoke(index);
+            }
             return;
         }
-        this.delta += (int) (DateTime.Now - this.lastUpdate).TotalMilliseconds;
+        
+        this.delta += (int)(DateTime.Now - this.lastUpdate).TotalMilliseconds;
 
         if (this.delta > this.Tempo)
         {
@@ -46,19 +58,23 @@ internal class PlaySongInfo
         }
         this.lastUpdate = DateTime.Now;
     }
+
     public void Play()
     {
         this.PlayCompleted = false;
         this.noteIndex = 0;
         this.delta = 0;
+        this.completedTriggered = false;
         this.lastUpdate = DateTime.Now;
     }
+
     public void Stop()
     {
         this.PlayCompleted = true;
     }
+
     public static void PlayNote(int index, float note)
     {
-        NetMessage.SendData((int) PacketTypes.PlayHarp, index, -1, null, index, note, 0f, 0f, 0);
+        NetMessage.SendData((int)PacketTypes.PlayHarp, index, -1, null, index, note, 0f, 0f, 0);
     }
 }
