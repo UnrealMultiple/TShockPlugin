6155e4dbf94b3b732f5636cda6c5a2eb8f5b536b
十七 <1653638943@qq.com>
2026-04-11 14:35:41 +0800

    refactor(RealTime): 精简部分代码，提升性能 (#1138)
    * update：重构部分代码，使其更精简，性能有所提升
    
    * chore: bump submodule
    
    ---------
    
    Co-authored-by: ACaiCat <13110818005@qq.com>

--- src/RealTime/RealTime.cs
+++ src/RealTime/RealTime.cs
@@ -1,5 +1,4 @@
-﻿using MonoMod.RuntimeDetour;
-using System.Reflection;
+﻿using System.Reflection;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
@@ -7,17 +6,35 @@ using TShockAPI;
 namespace RealTime;
 
 public delegate void ChangeCloud();
+
 [ApiVersion(2, 1)]
 public class RealTime : TerrariaPlugin
 {
+    #region 基础部分
     public override string Author => "十七";
     public override string Description => GetString("同步现实时间");
-    public override string Name => System.Reflection.Assembly.GetExecutingAssembly().GetName().Name!;
-    public override Version Version => new Version(2, 7, 0, 5);
+    public override string Name => Assembly.GetExecutingAssembly().GetName().Name!;
+    public override Version Version => new Version(2, 7, 0, 7);
     private static readonly Random rand = new Random();
+    private int realTimeSyncTimer = 0;   // 同步时间计时器
+    private int npcWeatherTimer = 0;     // NPC/渔夫/月相计时器
+    private int weatherChangeTimer = 0;  // 天气变化计时器
+    private int secondTimer = 0;         // 每秒触发一次的计时器
+    // 将事件的 DateTime 替换为简单的秒数倒计时
+    private int bloodMoonSecondsLeft = 0;
+    private int eclipseSecondsLeft = 0;
+    private int pumpkinMoonSecondsLeft = 0;
+    private int snowMoonSecondsLeft = 0;
+
+    private bool lastBloodMoon = false;
+    private bool lastEclipse = false;
+    private bool lastPumpkinMoon = false;
+    private bool lastSnowMoon = false;
+
     public RealTime(Main game) : base(game)
     {
     }
+
     public override void Initialize()
     {
         ServerApi.Hooks.GameUpdate.Register(this, this.OnGameUpdate);
@@ -25,6 +42,7 @@ public class RealTime : TerrariaPlugin
         GetDataHandlers.PlayerTeam += this.Team;
         On.Terraria.WorldGen.UnspawnTravelNPC += this.BlockUnspawn;
     }
+
     protected override void Dispose(bool disposing)
     {
         if (disposing)
@@ -36,23 +54,29 @@ public class RealTime : TerrariaPlugin
         }
         base.Dispose(disposing);
     }
+    #endregion
 
-    private void BlockUnspawn(On.Terraria.WorldGen.orig_UnspawnTravelNPC orig)//拦截旅商被删除的函数，旅商永久驻留
+    #region 禁止旅商离开
+    private void BlockUnspawn(On.Terraria.WorldGen.orig_UnspawnTravelNPC orig)
     {
         return;
     }
+    #endregion
 
-    private void Team(object? o, GetDataHandlers.PlayerTeamEventArgs args)//队伍判断
+    #region 禁止事件期间切换队伍
+    private void Team(object? o, GetDataHandlers.PlayerTeamEventArgs args)
     {
-        if (Main.bloodMoon == true || Main.eclipse == true || Main.pumpkinMoon == true || Main.snowMoon == true)
+        if (Main.bloodMoon || Main.eclipse || Main.pumpkinMoon || Main.snowMoon)
         {
             args.Player.SetTeam(0);
             args.Handled = true;
             args.Player.SendInfoMessage(GetString("事件禁止切换队伍。"));
         }
     }
+    #endregion
 
-    private void NPCS(On.Terraria.Main.orig_UpdateTime orig)//通过拦截时间更新，然后让其在非白天情况下npc入住
+    #region NPC夜间入住
+    private void NPCS(On.Terraria.Main.orig_UpdateTime orig)
     {
         orig();
         if (!Main.dayTime)
@@ -60,215 +84,178 @@ public class RealTime : TerrariaPlugin
             Main.UpdateTime_SpawnTownNPCs(true);
         }
     }
+    #endregion
 
-    public ChangeCloud GetRandom(ChangeCloud[] arr)//自定义随机数定义
+    #region 事件PvP逻辑
+    // 将频繁调用的强制PvP逻辑独立，现在它只会每秒被调用1次，而不是每秒60次
+    private void EnforceEventPvP(string warningMessage)
     {
-        var ran = new Random();
-        var n = ran.Next(arr.Length);
-        return arr[n];
+        foreach (var p in TShock.Players)
+        {
+            if (p != null && p.Active && !p.TPlayer.hostile)
+            {
+                p.SetTeam(0);
+                p.TPlayer.hostile = true;
+                p.SendData(PacketTypes.TogglePvp, "", p.Index);
+                p.SendInfoMessage(warningMessage);
+            }
+        }
     }
+    #endregion
 
-    int i = 0;
-    int y = 0;
-    int q = 0;
-    bool lastBloodMoon = false;
-    bool lastEclipse = false;
-    bool lastPumpkinMoon = false;
-    bool lastSnowMoon = false;
-    DateTime time = DateTime.Now;
+    #region 游戏更新逻辑
     private void OnGameUpdate(EventArgs args)
     {
-        #region 血月
-        if (this.lastBloodMoon ^ Main.bloodMoon)
+        //每秒触发一次的逻辑（60帧）: 用于倒计时和 PvP 检查
+        this.secondTimer++;
+        if (this.secondTimer >= 60)
         {
+            this.secondTimer = 0;
+
+            #region 血月倒计时与强制PvP
+            if (this.lastBloodMoon ^ Main.bloodMoon)
+            {
+                if (Main.bloodMoon) this.bloodMoonSecondsLeft = 20 * 60; // 20分钟换算为秒
+            }
+            this.lastBloodMoon = Main.bloodMoon;
             if (Main.bloodMoon)
             {
-                //血月开启瞬间
-                this.time = DateTime.Now + TimeSpan.FromMinutes(20);
-            }
-        }
-        this.lastBloodMoon = Main.bloodMoon;
-        if (Main.bloodMoon == true)
-        {
-            TShock.Players.Where(p => p != null).ToList().ForEach(p =>
-            {
-                if (p.TPlayer.hostile == false)
+                EnforceEventPvP(GetString("血月的邪恶影响会阻止你的PvP关闭。"));
+                this.bloodMoonSecondsLeft--;
+                if (this.bloodMoonSecondsLeft <= 0)
                 {
-                    p.SetTeam(0);
-                    p.TPlayer.hostile = true;
-                    p.SendData(PacketTypes.TogglePvp, "", p.Index);
-                    p.SendInfoMessage(GetString("血月的邪恶影响会阻止你的PvP关闭。"));
+                    Main.bloodMoon = false;
+                    NetMessage.SendData(7);
                 }
-            });
-            if (DateTime.Now >= this.time)
-            {
-                Main.bloodMoon = false;
             }
-        }
-        #endregion
-        #region 日食
-        if (this.lastEclipse ^ Main.eclipse)
-        {
+            #endregion
+
+            #region 日食倒计时与强制PvP
+            if (this.lastEclipse ^ Main.eclipse)
+            {
+                if (Main.eclipse) this.eclipseSecondsLeft = 20 * 60;
+            }
+            this.lastEclipse = Main.eclipse;
             if (Main.eclipse)
             {
-                //日食开启瞬间
-                this.time = DateTime.Now + TimeSpan.FromMinutes(20);
-            }
-        }
-        this.lastEclipse = Main.eclipse;
-        if (Main.eclipse == true)
-        {
-            TShock.Players.Where(p => p != null).ToList().ForEach(p =>
-            {
-                if (p.TPlayer.hostile == false)
+                EnforceEventPvP(GetString("日食的邪恶影响会阻止你的PvP关闭。"));
+                this.eclipseSecondsLeft--;
+                if (this.eclipseSecondsLeft <= 0)
                 {
-                    p.TPlayer.hostile = true;
-                    p.SetTeam(0);
-                    p.SendData(PacketTypes.TogglePvp, "", p.Index);
-                    p.SendInfoMessage(GetString("日食的邪恶影响会阻止你的PvP关闭。"));
+                    Main.eclipse = false;
+                    NetMessage.SendData(7);
                 }
-            });
-            if (DateTime.Now >= this.time)
-            {
-                Main.eclipse = false;
             }
-        }
-        #endregion
-        #region 万圣节
-        if (this.lastPumpkinMoon ^ Main.pumpkinMoon)
-        {
+            #endregion
+
+            #region 万圣节倒计时与强制PvP
+            if (this.lastPumpkinMoon ^ Main.pumpkinMoon)
+            {
+                if (Main.pumpkinMoon) this.pumpkinMoonSecondsLeft = 40 * 60;
+            }
+            this.lastPumpkinMoon = Main.pumpkinMoon;
             if (Main.pumpkinMoon)
             {
-                //万圣节开启瞬间
-                this.time = DateTime.Now + TimeSpan.FromMinutes(40);
-            }
-        }
-        this.lastPumpkinMoon = Main.pumpkinMoon;
-        if (Main.pumpkinMoon == true)
-        {
-            if (DateTime.Now >= this.time)
-            {
-                TShock.Players.Where(p => p != null).ToList().ForEach(p =>
+                EnforceEventPvP(GetString("万圣节的邪恶影响会阻止你的PvP关闭。"));
+                this.pumpkinMoonSecondsLeft--;
+                if (this.pumpkinMoonSecondsLeft <= 0)
                 {
-                    if (p.TPlayer.hostile == false)
-                    {
-                        p.TPlayer.hostile = true;
-                        p.SetTeam(0);
-                        p.SendData(PacketTypes.TogglePvp, "", p.Index);
-                        p.SendInfoMessage(GetString("万圣节的邪恶影响会阻止你的PvP关闭。"));
-                    }
-                });
-                Main.pumpkinMoon = false;
+                    Main.pumpkinMoon = false;
+                    NetMessage.SendData(7);
+                }
             }
-        }
-        #endregion
-        #region 霜月
-        if (this.lastSnowMoon ^ Main.snowMoon)
-        {
+            #endregion
+
+            #region 霜月倒计时与强制PvP
+            if (this.lastSnowMoon ^ Main.snowMoon)
+            {
+                if (Main.snowMoon) this.snowMoonSecondsLeft = 40 * 60;
+            }
+            this.lastSnowMoon = Main.snowMoon;
             if (Main.snowMoon)
             {
-                //霜月开启瞬间
-                this.time = DateTime.Now + TimeSpan.FromMinutes(40);
-            }
-        }
-        this.lastSnowMoon = Main.snowMoon;
-        if (Main.snowMoon == true)
-        {
-            if (DateTime.Now >= this.time)
-            {
-                TShock.Players.Where(p => p != null).ToList().ForEach(p =>
+                EnforceEventPvP(GetString("霜月的邪恶影响会阻止你的PvP关闭。"));
+                this.snowMoonSecondsLeft--;
+                if (this.snowMoonSecondsLeft <= 0)
                 {
-                    if (p.TPlayer.hostile == false)
-                    {
-                        p.TPlayer.hostile = true;
-                        p.SetTeam(0);
-                        p.SendData(PacketTypes.TogglePvp, "", p.Index);
-                        p.SendInfoMessage(GetString("霜月的邪恶影响会阻止你的PvP关闭。"));
-                    }
-                });
-                Main.snowMoon = false;
+                    Main.snowMoon = false;
+                    NetMessage.SendData(7);
+                }
             }
+            #endregion
         }
-        #endregion
+
         #region 真实时间
-        this.i++;
-        if (this.i == 480)//真实时间
+        //每480帧 / 约8秒触发
+        this.realTimeSyncTimer++;
+        if (this.realTimeSyncTimer >= 480)
         {
             var dt = DateTime.Now;
-            dt.ToShortTimeString().ToString();
-            var d = int.Parse(dt.Hour.ToString()) + (int.Parse(dt.Minute.ToString()) / 60.0m);
-            d -= 4.50m;
-            if (d < 0.00m)
-            {
-                d += 24.00m;
-            }
-            if (d >= 15.00m)
-            {
-                TSPlayer.Server.SetTime(false, (double) ((d - 15.00m) * 3600.0m));
-            }
+            double d = dt.Hour + (dt.Minute / 60.0);
+            d -= 4.5;
+            if (d < 0.0) d += 24.0;
+
+            if (d >= 15.0)
+                TSPlayer.Server.SetTime(false, (d - 15.0) * 3600.0);
             else
-            {
-                TSPlayer.Server.SetTime(true, (double) (d * 3600.0m));
-            }
-            this.i = 0;
+                TSPlayer.Server.SetTime(true, d * 3600.0);
+
+            this.realTimeSyncTimer = 0;
         }
         #endregion
+
         #region npc生成、月相、天气、渔夫任务刷新
-        this.y++; 
-        if (this.y == 86400)//npc生成 月相、天气、渔夫任务刷新
+        //每86400帧/约24分钟
+        this.npcWeatherTimer++;
+        if (this.npcWeatherTimer >= 86400)
         {
-            var AllNPCS = Main.npc.Where(n => n != null);
-            foreach (var TNPC in AllNPCS)
+            bool oldManAlive = false;
+            bool cultistAlive = false;
+            for (int j = 0; j < Main.maxNPCs; j++)//检查所有NPC是否存在老人或教徒
             {
-                if (TNPC.netID == 37)
+                var npc = Main.npc[j];
+                if (npc != null && npc.active)
                 {
-                    if (!TNPC.active)//遍历所有NPC，判断这个NPC是否存在
-                    {
-                        if (!NPC.downedBoss3)//判断有没有击败骷髅王
-                        {
-                            TSPlayer.Server.SpawnNPC(37, "老人", 200, Main.dungeonX, Main.dungeonY, 50, 20);
-                        }
-                    }
-                }
-                if (TNPC.netID == 439)
-                {
-                    if (!TNPC.active)
-                    {
-                        if (!NPC.downedMoonlord && NPC.downedGolemBoss)//没有击败月总但击败了石头人
-                        {
-                            TSPlayer.Server.SpawnNPC(439, "教徒", 200, Main.dungeonX, Main.dungeonY, 50, 20);
-                        }
-                    }
+                    if (npc.netID == 37) oldManAlive = true;
+                    if (npc.netID == 439) cultistAlive = true;
                 }
             }
-            Chest.SetupTravelShop();
+            if (!oldManAlive && !NPC.downedBoss3)//如果没有老人且没有打过骷髅王，就生成老人
+                TSPlayer.Server.SpawnNPC(37, "老人", 1, Main.dungeonX, Main.dungeonY, 50, 20);
+            if (!cultistAlive && !NPC.downedMoonlord && NPC.downedGolemBoss)//如果没有教徒且打过石巨人但没打过月球领主，就生成教徒
+                TSPlayer.Server.SpawnNPC(439, "教徒", 1, Main.dungeonX, Main.dungeonY, 50, 20);
+
+            Chest.SetupTravelShop();//更新旅商商品
             NetMessage.SendData(72);
-            Main.AnglerQuestSwap();//更换渔夫任务
+            Main.AnglerQuestSwap();//更新渔夫任务
             TSPlayer.All.SendInfoMessage(GetString("渔夫任务和旅商商品已更换"));
-            if (DateTime.Now.Hour >= 19 || DateTime.Now.Hour <= 4)
+
+            int currentHour = DateTime.Now.Hour; 
+            if (currentHour >= 19 || currentHour <= 4)
             {
                 int nextMoon = (Main.moonPhase + 1) % 8;
-                var msg = this.GetMoon(nextMoon);
                 Main.moonPhase = nextMoon;
                 NetMessage.SendData(7);
-                TSPlayer.All.SendInfoMessage(GetString($"月相已更换为：{msg}"));
+                TSPlayer.All.SendInfoMessage(GetString($"月相已更换为：{this.GetMoon(nextMoon)}"));
             }
-            this.y = 0;
+            this.npcWeatherTimer = 0;
         }
         #endregion
+
         #region 天气刷新
-        this.q++;
-        if (this.q == 36000)//天气刷新
+        //每36000帧/约10分钟
+        this.weatherChangeTimer++;
+        if (this.weatherChangeTimer >= 36000)
         {
             // 先清空所有天气
             Main.StopRain();
             Terraria.GameContent.Events.Sandstorm.StopSandstorm();
             Main.StopSlimeRain();
-            // 风归零
+            //风速重置
             Main.windSpeedTarget = 0f;
             Main.windSpeedCurrent = 0f;
-            int r = rand.Next(100); // 权重随机
-            string weatherName = "";
+            int r = rand.Next(100);//随机生成0-99的数来决定天气
+            string weatherName;
             if (r < 35)
             {
                 Main.numClouds = 0;
@@ -310,12 +297,16 @@ public class RealTime : TerrariaPlugin
                 Main.windSpeedCurrent = 0.7f;
                 weatherName = GetString("沙尘暴");
             }
+
             NetMessage.SendData(7);
             TSPlayer.All.SendInfoMessage(GetString($"当前天气：{weatherName}"));
-            this.q = 0;
+            this.weatherChangeTimer = 0;
         }
         #endregion
     }
+    #endregion
+
+    #region 月相文本
     private string GetMoon(int phase)
     {
         switch (phase)
@@ -331,4 +322,5 @@ public class RealTime : TerrariaPlugin
             default: return GetString("未知");
         }
     }
+    #endregion
 }
